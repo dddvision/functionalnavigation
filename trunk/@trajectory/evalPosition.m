4568cb24202920100edc71f14359139048aad18e
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2009-07-20 17:16:20 +0000

    added comments related to trajectory interpretation

--- trunk/@trajectory/evalPosition.m
+++ trunk/@trajectory/evalPosition.m
@@ -1,7 +1,14 @@
 % Return trajectory position components at several time instants
+%
+% INPUTS
+% x = trajectory object
+% t = time at which to evaluate the trajectory
+%
+% OUTPUT
+% pt = position at the given time instant 
 
 
-function pt=evalPosition(this,t)
-pqt=eval(this,t);
+function pt=evalPosition(x,t)
+pqt=eval(x,t);
 pt=pqt(1:3,:);
 return;
