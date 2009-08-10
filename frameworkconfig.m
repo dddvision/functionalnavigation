f328b42b6cc3385b1ba64efc8333940dcfd64eb9
dddvision <dddvision@gmail.com>
2009-08-10 17:07:25 +0000

    separated components into "trajectories", "sensors" and "optimizers"

--- frameworkconfig.m
+++ frameworkconfig.m
@@ -2,7 +2,9 @@ function config=frameworkconfig
 
 % TODO: support configuration of multiple sensors
 mainpath=fileparts(which('main'));
-config.componentspath=fullfile(mainpath,'components');
+config.trajectoryComponentPath=fullfile(mainpath,'trajectories');
+config.sensorComponentPath=fullfile(mainpath,'sensors');
+config.optimizerComponentPath=fullfile(mainpath,'optimizers');
 config.trajectory='trajectorystub'; % try 'linewobble1','pendulum1'
 config.sensor='sensorstub'; % try 'cameraOpticalFlow1'
 config.optimizer='optimizerstub'; % NO OPTIMIZATION METHODS IMPLEMENTED YET
