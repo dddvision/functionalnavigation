a6ea0f72d5a3c4bda2b0d8b1e329a05b1c4f3196
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2009-08-10 17:57:05 +0000

    steps toward integrating the matlab GA optimizer

--- trunk/optimizers/@matlabGA1/step.m
+++ trunk/optimizers/@matlabGA1/step.m
@@ -1,33 +1,9 @@
-% Execute one step of the optimizer
-%
-% INPUT/OUTPUT
-% M = optimizer object
-% H = objective object
-% v = dynamic seed, M-by-popsize
-% w = static seed, N-by-popsize
-%
-% NOTES
-% The primary purpose of this function is to evolve better seeds {v,w}
-% by implicitly calling the evaluate() function of the objective object.
-%
-% H = The objective object is passed through eval() but
-% is otherwise unmodified by step().
-%
-% This function may modify the state of its first arg.
-
-
 function [M,H,v,w]=step(M,H,v,w)
-
-switch( M.optimizer )
-  case 'matlabga'
-    problem=ga_fitness_wrapper('put',H,v,w);
-    [junk1,junk2,junk3,junk4,population,score]=ga(problem);
-    [H,v,w,c]=ga_fitness_wrapper('get',population,score);
-  otherwise
-    [H,c]=evaluate(H,v,w);
-end
-    
-fprintf('\n\ncost summary:\n');
-disp(c');
-
+problem=ga_fitness_wrapper('put',H,v,w);
+[junk1,junk2,junk3,junk4,population,score]=ga(problem);
+[H,v,w,c]=ga_fitness_wrapper('get',population,score);
+fprintf('\n');
+fprintf('\ncost summary:');
+fprintf('\n%f',c);
+fprintf('\n');
 end
--- trunk/optimizers/@optimizerstub/step.m
+++ trunk/optimizers/@optimizerstub/step.m
@@ -1,20 +1,3 @@
-% Execute one step of the optimizer
-%
-% INPUT/OUTPUT
-% H = objective object
-% v = dynamic seed, M-by-popsize
-% w = static seed, N-by-popsize
-%
-% NOTES
-% The primary purpose of this function is to evolve better seeds {v,w}
-% by implicitly calling the evaluate() function of the objective object.
-%
-% H = The objective object is passed through eval() but
-% is otherwise unmodified by step().
-%
-% This function can modify the object state.
-
-
 function [this,H,v,w]=step(this,H,v,w)
 [H,c]=evaluate(H,v,w);
 fprintf('\n');
