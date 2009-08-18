b86519506de649cce9b4c93e77b36ddd04fb5702
dddvision <dddvision@gmail.com>
2009-08-18 21:31:49 +0000

    minor changes in preparation for noise negotiation update

--- frameworkconfig.m
+++ frameworkconfig.m
@@ -5,6 +5,7 @@ mainpath=fileparts(which('main'));
 config.trajectoryComponentPath=fullfile(mainpath,'trajectories');
 config.sensorComponentPath=fullfile(mainpath,'sensors');
 config.optimizerComponentPath=fullfile(mainpath,'optimizers');
+
 config.trajectory='trajectorystub'; % try 'linewobble1','pendulum1'
 config.sensor='sensorstub'; % try 'cameraOpticalFlow1'
 config.optimizer='optimizerstub'; % try 'matlabGA1'
