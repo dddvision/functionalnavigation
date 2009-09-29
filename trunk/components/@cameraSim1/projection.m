dc038e65724b9ab0d9d774158eb35e5f6097ad63
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2009-09-29 21:20:57 +0000

    vectorized camera projection and inverse projection

--- trunk/components/@cameraSim1/projection.m
+++ trunk/components/@cameraSim1/projection.m
@@ -4,7 +4,9 @@ function xy=projection(this,k,ray)
   rho=cameraSim1_singleton.ring{buf}.rho;
   m=cameraSim1_singleton.ring{buf}.size(1);
   n=cameraSim1_singleton.ring{buf}.size(2);
-  u=rho/ray(1)*[ray(3);ray(2)];
-  xy=[(u(2)+1)*((n-1)/2);
-      (u(1)+1)*((m-1)/2)];
+  a=rho/ray(1,:);
+  u1=a.*ray(3,:);
+  u2=a.*ray(2,:);
+  xy=[(u2+1)*((n-1)/2);
+      (u1+1)*((m-1)/2)];
 end
