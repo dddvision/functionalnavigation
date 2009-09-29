0a74eb6345bb994a84695cc35fee5618c054f2ed
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2009-09-29 04:13:45 +0000

    updated camera, sensor, and trajectory definitions

--- trunk/camera.m
+++ trunk/camera.m
@@ -6,42 +6,82 @@ classdef camera < sensor
   end
   
   methods (Access=public,Abstract=true)
-    % Get grayscale image from a node
+    % Get grayscale image
     %
     % INPUT
-    % k = integer index, uint32 scalar
+    % k = node index, uint32 scalar
     %
     % OUTPUT
     % gray = grayscale image, uint8 M-by-N
     %
     % NOTE
     % If no image is available, then return value is 0-by-0
-    % TODO: use exceptions for error handling
+    % TODO: define exceptions for error handling
     gray=getGray8RowMajor(this,k);
     gray=getGray8ColMajor(this,k);
     
-    % Get color image from a node
+    % Get color image
     %
     % INPUT
-    % k = integer index, uint32 scalar
+    % k = node index, uint32 scalar
     %
     % OUTPUT
     % rgb = color image, uint8 M-by-N-by-3
     %
     % NOTE
     % If no image is available, then return value is 0-by-0-by-3
-    % TODO: use exceptions for error handling
+    % TODO: define exceptions for error handling
     rgb=getColor32RowMajor(this,k);
     rgb=getColor32ColMajor(this,k);
+
     
-    % Get focal length from a node
+    %%% GETFOCAL IS LIKELY TO BE DEPRICATED %%%
+    
+    
+    % Get focal length
     %
     % INPUT
-    % k = integer index, uint32 scalar
+    % k = node index, uint32 scalar
     %
     % OUTPUT
     % rho = focal length, double scalar
+    % TODO: provide intrinsic calibration data or forward/reverse projection functions
+    % TODO: define exceptions for error handling
     rho=getFocal(this,k);
+    
+    
+    %%% THE FOLLOWING ARE UNDER CONSIDERATION %%%
+
+
+    % Get camera frame represented in the body frame
+    %
+    % INPUT
+    % k = node index, uint32 scalar
+    %
+    % OUTPUT
+    % posquat = position and quaternion transformation, double 7-by-1
+    % TODO: define exceptions for error handling
+    % posquat=gimbalState(this,k);
+    
+    % Transform ray vectors in the camera frame to image points
+    %
+    % INPUT
+    % k = node index, uint32 scalar
+    % ray = unit vector in camera frame, double 3-by-1
+    %
+    % OUTPUT
+    % ij = point in image coordinates, double 2-by-1
+    % ij=project(this,k,ray);
+    
+    % Transform image points to ray vectors in the camera frame
+    %
+    % INPUT
+    % k = node index, uint32 scalar
+    % ij = point in image coordinates, double 2-by-1
+    %
+    % OUTPUT
+    % ray = unit vector in camera frame, double 3-by-1
+    % ray=inverseProject(this,k,ij);
   end
  
 end
