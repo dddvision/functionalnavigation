e5303e5f6c539e94e7678e93f64c733273516d8f
dddvision <dddvision@gmail.com>
2009-09-30 17:34:35 +0000

    removed middlebury data from repository (now downloaded on demand)

--- components/@cameraSim1/cameraSim1.m
+++ components/@cameraSim1/cameraSim1.m
@@ -10,19 +10,15 @@ classdef cameraSim1 < camera
         % H. Hirschmuller and D. Scharstein. Evaluation of cost functions for 
         % stereo matching. In IEEE Computer Society Conference on Computer Vision 
         % and Pattern Recognition (CVPR 2007), Minneapolis, MN, June 2007.
-        cameraSim1_singleton.ring{1}.time=1.6;
-        cameraSim1_singleton.ring{1}.image=file2rgb('view2.png');
-        cameraSim1_singleton.ring{1}.rho=1;
-        cameraSim1_singleton.ring{2}.time=1.2;
-        cameraSim1_singleton.ring{2}.image=file2rgb('view0.png');
-        cameraSim1_singleton.ring{2}.rho=1;
-        cameraSim1_singleton.ring{3}.time=1.4;
-        cameraSim1_singleton.ring{3}.image=file2rgb('view1.png');
-        cameraSim1_singleton.ring{3}.rho=1;
-        cameraSim1_singleton.ringsz=uint32(3);     
-        cameraSim1_singleton.base=uint32(2);
+        for k=1:7
+          cameraSim1_singleton.ring{k}.time=1+k/10;
+          cameraSim1_singleton.ring{k}.image=getMiddleburyArt(k-1);
+        end
+        cameraSim1_singleton.rho=1;
+        cameraSim1_singleton.ringsz=uint32(numel(cameraSim1_singleton.ring));     
+        cameraSim1_singleton.base=uint32(1);
         cameraSim1_singleton.a=uint32(3);
-        cameraSim1_singleton.b=uint32(5);
+        cameraSim1_singleton.b=uint32(9);
         cameraSim1_singleton.size=size(cameraSim1_singleton.ring{1}.image);
       end
     end    
