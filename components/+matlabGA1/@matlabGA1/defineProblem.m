a409e309c100d8913e1d3c1048da74b78a9998f4
dddvision <dddvision@gmail.com>
2009-10-07 19:36:08 +0000

    moved matlabGA1 out of @ directory

--- components/+matlabGA1/@matlabGA1/defineProblem.m
+++
@@ -1,6 +0,0 @@
-function [this,initialCost]=defineProblem(this,objectiveFunction,initialParameters)
-  initialCost=feval(objectiveFunction,initialParameters);
-  this.objective=objectiveFunction;
-  this.parameters=initialParameters;
-  this.cost=initialCost;
-end
--- components/+matlabGA1/matlabGA1.m
+++ components/+matlabGA1/matlabGA1.m
@@ -52,6 +52,28 @@ classdef matlabGA1 < optimizer
       this.stepGAhandle=@stepGA;
       cd(pathtemp);
     end
+    
+    function [this,initialCost]=defineProblem(this,objectiveFunction,initialParameters)
+      initialCost=feval(objectiveFunction,initialParameters);
+      this.objective=objectiveFunction;
+      this.parameters=initialParameters;
+      this.cost=initialCost;
+    end
+    
+    % Requires a license for the Matlab GADS toolbox
+    function [this,parameters,cost]=step(this)
+      % TODO: move these lines to defineProblem?
+      [popsize,nvars]=size(this.parameters);
+      options=this.defaultOptions;
+      options.PopulationSize=popsize;
+      options.EliteCount=max(1,popsize/20);
+      nullstate=struct('FunEval',0);
+      %nullobjective=@(x) zeros(size(x,1),1);
+      
+      [cost,parameters]=feval(this.stepGAhandle,this.cost,this.parameters,options,nullstate,nvars,this.objective);
+      this.parameters=parameters;
+      this.cost=cost;
+    end
   end
   
 end
