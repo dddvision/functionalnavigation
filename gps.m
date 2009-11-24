40867b67b43fce0677cca8eea5ec3d67a140d1c6
dddvision <dddvision@gmail.com>
2009-11-24 05:34:31 +0000

    added opticalFlow configuration files minor updates to gps.m

--- gps.m
+++ gps.m
@@ -1,47 +1,47 @@
-% This class defines a single gps sensor 
+% This class defines a single global positioning sensor 
 % If you need to add optional device methods, then inherit from this class
 % Using SI units (meters, seconds, radians)
 classdef gps < sensor
     
   methods (Abstract=true)
-    % Get sensor position relative to the body frame
+    % Get sensor offset relative to the body frame
     %
     % INPUT
-    % k = zero-based sensor index, uint32 scalar
+    % k = data index, uint32 scalar
     %
     % OUTPUT
-    % offset = position of origin, double 3-by-1
+    % offset = position of sensor origin, double 3-by-1
     % direction = unit normalized direction vector, 3-by-1
-    offset=getSensorPos(this,k);
+    offset = getOffset(this,k);
     
     % Get a position measurement
     %
     % INPUT
-    % k = zero-based sensor index, uint32 scalar
+    % k = data index, uint32 scalar
     %
     % OUTPUT
     % lon = longitude in radians, double scalar
     % lat = latitude in  radians, double scalar
     % alt = altitude above WGS84 ellipsoid in meters, double scalar
     [lon,lat,alt] = getPosition(this,k);
-    
-    % Get GPS precision information %
-    % INPUT
-    % k = zero-based sensor index, uint32 scalar
-    %
-    % OUTPUT 
-    % hDop = horizontal dilution  of precision (unitless), double scalar
-    % vDop = vertical dilution of precision (unitless), double scalar
-    % sigmaR = standard deviation for range error (meters), double scalar
-    [hDop,vDop, sigmaR] = getPrecision(this, k);
-    
+ 
     % Check whether precision information is available
     % 
     % OUTPUT
     % flag = true if precision data is available from the sensor, 
     %        false otherwise, bool
     flag = hasPrecision(this);
-   
+    
+    % Get precision information
+    %
+    % INPUT
+    % k = data index, uint32 scalar
+    %
+    % OUTPUT 
+    % hDop = horizontal dilution  of precision (unitless), double scalar
+    % vDop = vertical dilution of precision (unitless), double scalar
+    % sigmaR = scale of equivalent spherical error (meters), double scalar
+    [hDop,vDop,sigmaR] = getPrecision(this,k);
   end 
  
 end
