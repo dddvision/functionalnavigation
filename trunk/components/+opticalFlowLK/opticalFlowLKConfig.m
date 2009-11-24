82a9733ae421327878b5589a9c578b8e7c857fe7
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2009-11-24 05:34:31 +0000

    added opticalFlow configuration files minor updates to gps.m

---
+++ trunk/components/+opticalFlowLK/opticalFlowLKConfig.m
@@ -0,0 +1,5 @@
+classdef opticalFlowLKConfig
+  properties (Constant=true)
+    windowSize=3;
+  end
+end
---
+++ trunk/components/+opticalFlowPyramid/opticalFlowPyramidConfig.m
@@ -0,0 +1,7 @@
+classdef opticalFlowPyramidConfig
+  properties (Constant=true)
+    windowSize=3; 
+    numLevels=3; 
+    iterations=3; 
+  end  
+end
