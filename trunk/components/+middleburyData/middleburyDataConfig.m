c97a8dcb47dae4a50415779bbae45da445a85247
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2009-12-14 18:37:57 +0000

    The middleburyData component can now access multiple middlebury data sets, see middleburyDataConfig.mThe middleburyData component can now access multiple middlebury data sets, see middleburyDataConfig.m

---
+++ trunk/components/+middleburyData/middleburyDataConfig.m
@@ -0,0 +1,9 @@
+classdef middleburyDataConfig  < handle
+  properties (Constant=true)
+    sceneYear = 'scenes2005'; % 'scenes2005', 'scenes2006'
+    fractionalSize = 'ThirdSize'; % 'FullSize', 'HalfSize', 'ThirdSize'   
+    scene = 'Art'; % 'Art', 'Books', 'Aloe', ...}
+    illumination = 'Illum2'; % 'Illum1', 'Illum2', 'Illum3'
+    exposure = 'Exp1'; % 'Exp0', 'Exp1', 'Exp2'
+  end
+end
