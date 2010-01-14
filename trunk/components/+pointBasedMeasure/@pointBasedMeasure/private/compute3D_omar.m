dcedfee69ccd4dff2acb354cce9f364052712b8f
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-01-14 21:07:11 +0000

    moved opticalFlowPdollar, opticalFlowOpenCV, and pointBasedMeasure to https://svn.ssci.com/repos/functionalnavigation

--- trunk/components/+pointBasedMeasure/@pointBasedMeasure/private/compute3D_omar.m
+++
@@ -1,9 +0,0 @@
-% compute 3D
-function X = compute3D_omar(x1,x2,P1,P2)
-A = [x1(1).*P1(3,:) - P1(1,:);
-    x1(2).*P1(3,:) - P1(2,:);
-    x2(1).*P2(3,:) - P2(1,:);
-    x2(2).*P2(3,:) - P2(2,:)];
-
-[U,S,V] = svd(A);
-X = V(:,end);
