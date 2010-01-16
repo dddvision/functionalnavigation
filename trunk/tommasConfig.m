b56f009f1f6ddec2f3fdad88ba0f18a77cabf171
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-01-16 02:21:31 +0000

    minor display improvement configuration points to opticalFlowOpenCV by default

--- trunk/tommasConfig.m
+++ trunk/tommasConfig.m
@@ -11,7 +11,7 @@ classdef tommasConfig < handle
     dataURI = 'matlab:middleburyData.middleburyData';
         
     % select measures
-    measures = {'pointBasedMeasure'};
+    measures = {'opticalFlowOpenCV'};
   end
     
 end
