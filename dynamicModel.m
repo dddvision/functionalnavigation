204e376975930a8e7109ad507c47efc8bc7d3c0d
dddvision <dddvision@gmail.com>
2010-03-06 05:27:33 +0000

    Major changes to dynamicModel class. Dynamic models no longer depend on the optimizer to supply the full initial state. Instead, they provide an initial parameter block and associated cost function, such that the initial state can be sampled from a distribution over a reduced parameter set. The class definition and components have been updated to reflect this change.

--- dynamicModel.m
+++ dynamicModel.m
@@ -1,10 +1,14 @@
 % This class augments a trajectory with defining parameters
 %
-% NOTE
+% NOTES
 % This class depends on parameter blocks of the following form
-%   block.logical = logical parameters, logical numLogical-by-1
-%   block.uint32 = unsigned integer parameters, uint32 numUint32-by-1
+%   description.numLogical = number of 1-bit logical parameters, uint32 scalar
+%   description.numUint32 = number of 32-bit unsigned integer parameters, uint32 scalar
+%   block.logical = logical parameters, logical 1-by-numLogical
+%   block.uint32 = unsigned integer parameters, uint32 1-by-numUint32
+% There are seperate block descriptions for initial and extension blocks
 % The range of uint32 is [0,4294967295]
+% Parameters may be treated as range-bounded doubles via static casting
 classdef dynamicModel < trajectory
   
   properties (Constant=true,GetAccess=public)
@@ -17,46 +21,56 @@ classdef dynamicModel < trajectory
     % INPUT
     % uri = uniform resource identifier, string
     % initialTime = initial lower bound of the trajectory domain, double scalar
+    % initialBlock = (see above), struct scalar
     %
     % NOTES
     % The URI should identify a hardware resource or dataContainer
     % URI examples:
     %   'file://dev/camera0'
-    %   'matlab:middleburyData.middleburyData'    
-    % The default body state at the initial time is at the origin:
-    %   position = [0;0;0];
-    %   rotation = [1;0;0;0];
-    %   positionRate = [0;0;0];
-    %   rotationRate = [0;0;0;0];
+    %   'matlab:middleburyData.middleburyData'
     % Each subclass constructor must pass identical arguments to this 
-    %   constructor using the syntax this=this@dynamicModel(uri,initialTime);
-    function this=dynamicModel(uri,initialTime)
+    %   constructor using the syntax this=this@dynamicModel(uri,initialTime,initialBlock);
+    function this=dynamicModel(uri,initialTime,initialBlock)
       assert(isa(uri,'char'));
       assert(isa(initialTime,'double'));
+      assert(isa(initialBlock,'struct'));
     end    
   end
   
   methods (Abstract=true,Static=true,Access=public)
-    % Get a description of a single block of parameters
+    % Get description of the initial block
     %
     % OUTPUT
-    % description.numLogical = number of 1-bit logical parameters, uint32 scalar
-    % description.numUint32 = number of 32-bit unsigned integer parameters, uint32 scalar
-    %
-    % NOTES
-    % Unsigned integers may be treated as range-bounded doubles via static casting
-    description=getBlockDescription;
+    % description = (see above), struct scalar
+    description=getInitialBlockDescription;
     
-    % Get the conversion between number of blocks and associated time domain extension
+    % Get description of a extension block
+    %
+    % OUTPUT
+    % description = (see above), struct scalar
+    description=getExtensionBlockDescription;
+    
+    % Get the conversion between number of extension blocks and associated time domain extension
     %
     % OUTPUT
     % blocksPerSecond = each block will extend the domain the reciprical of this rate, double scalar
     blocksPerSecond=getUpdateRate;
-    
-    % Compute the cost associated with a parameter block
+  end
+  
+  methods (Abstract=true,Access=public)
+    % Replace the initial block
     %
     % INPUT
-    % block = single parameter block, struct scalar
+    % initialBlock = (see above), struct scalar
+    %
+    % NOTES
+    % This function modifies the object instance
+    replaceInitialBlock(this,initialBlock);
+    
+    % Compute the cost associated with the initial block
+    %
+    % INPUT
+    % initialBlock = (see above), struct scalar
     %
     % OUTPUT
     % cost = non-negative cost associated with the block, double scalar
@@ -64,46 +78,47 @@ classdef dynamicModel < trajectory
     % NOTE
     % Output is analogous to mahalanobis distance
     % Typical values are in the range [0,3]
-    cost=computeBlockCost(block);
-  end
-  
-  methods (Abstract=true,Access=public)
-    % Get the total number of parameter blocks
+    cost=computeInitialBlockCost(this,initialBlock);
+    
+    % Get the total number of extension blocks
     %
     % OUTPUT
-    % numBlocks = total number of blocks, uint32 scalar
-    numBlocks=getNumBlocks(this);
+    % num = total number of extension blocks, uint32 scalar
+    num=getNumExtensionBlocks(this);
     
-    % Set the body state at the initial time
-    %
-    % INPUT
-    % See output of trajectory.evaluate()
-    %
-    % NOTES
-    % This function modifies the object instance
-    setInitialState(this,position,rotation,positionRate,rotationRate);
-    
-    % Replace multiple blocks of parameters
+    % Replace multiple extension blocks
     %
     % INPUT
     % k = zero-based indices of block locations sorted in ascending order, uint32 N-by-1
-    % blocks = parameter blocks, struct N-by-1
+    % blocks = (see above), struct N-by-1
     %
     % NOTES
     % Unsigned integers may be treated as range-bounded doubles via static casting
     % This function modifies the object instance
     % Throws an exception if any index is is invalid
-    replaceBlocks(this,k,blocks);
+    replaceExtensionBlocks(this,k,blocks);
 
-    % Extend the time domain by appending consecutive blocks of parameters
+    % Extend the time domain by appending consecutive extension blocks
     %
     % INPUT
-    % blocks = parameter blocks, struct M-by-1
+    % blocks = (see above), struct M-by-1
     %
     % NOTES
-    % Unsigned integers may be treated as range-bounded doubles via static casting
     % This function modifies the object instance
-    appendBlocks(this,blocks);
+    appendExtensionBlocks(this,blocks);
+    
+    % Compute the cost associated with an extension block
+    %
+    % INPUT
+    % block = (see above), struct scalar
+    %
+    % OUTPUT
+    % cost = non-negative cost associated with the block, double scalar
+    %
+    % NOTE
+    % Output is analogous to mahalanobis distance
+    % Typical values are in the range [0,3]
+    cost=computeExtensionBlockCost(this,block);
   end
     
 end
