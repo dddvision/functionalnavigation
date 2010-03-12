c1042cc8719eec58edfbba41fd717b6f190dfee7
dddvision <dddvision@gmail.com>
2010-03-12 23:24:33 +0000

    More minor tweaks going toward the linearKalman components in the branches directory.

--- mainDisplayConfig.m
+++ mainDisplayConfig.m
@@ -8,7 +8,7 @@ classdef mainDisplayConfig < handle
     scale=0.01; % (0.01) scale of body frame
     bigSteps=10; % (10) number of body frames per trajectory
     subSteps=10; % (10) number of line segments between body frames
-    infinity=1e6; % (1e6) replaces infinity as a time domain upper bound
+    infinity=10; % (1e6) replaces infinity as a time domain upper bound
     colorBackground = [1,1,1]; % ([1,1,1]) color of figure background
     colorHighlight = [1,0,0]; % ([1,0,0]) color of objects to emphasize
     colorReference = [0,1,0]; % ([0,1,0]) color of reference objects
