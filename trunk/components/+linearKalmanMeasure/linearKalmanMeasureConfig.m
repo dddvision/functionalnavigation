a4eabd776d332417e30c57efe6e0993fe5086362
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-03-23 14:20:34 +0000

    Minor changes to linearKalman components regarding computation of cost and hessian.

--- trunk/components/+linearKalmanMeasure/linearKalmanMeasureConfig.m
+++ trunk/components/+linearKalmanMeasure/linearKalmanMeasureConfig.m
@@ -1,6 +1,6 @@
 classdef linearKalmanMeasureConfig < handle
   properties (Constant=true)
-    dt=0.001; % ASSUMPTION: fixed known time step
-    sigma=0.5; % ASSUMPTION: fixed known noise parameter
+    dt=0.001; % fixed time step
+    deviation=0.1; % deviation of measurement noise
   end
 end
--- trunk/components/+linearKalmanOptimizer/linearKalmanOptimizerConfig.m
+++ trunk/components/+linearKalmanOptimizer/linearKalmanOptimizerConfig.m
@@ -1,6 +1,6 @@
 classdef linearKalmanOptimizerConfig < handle
   properties (Constant=true)
     referenceTime=0; % (0) initial time reference
-    plotDistributions=true; % (false) plots normal distributions when true
+    plotDistributions=false; % (false) plots normal distributions when true
   end
 end
