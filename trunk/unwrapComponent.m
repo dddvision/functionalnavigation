e06781c429f88fb6f98fee637fce973dbedbfa70
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-03-30 17:25:19 +0000

    Rearranged code in pursuit of creating a comprehensive TOMMAS testbed.

--- trunk/unwrapComponent.m
+++ trunk/unwrapComponent.m
@@ -1,14 +1,15 @@
 % Turn a package identifier into an object
 %
 % INPUT
-% pkg = package identifier (directory name without '+' prefix), string
+% pkg = package identifier, string
 % varargin = arguments for the class constructor
 %
 % OUTPUT
 % obj = object instance, class determined by pkg
 %
 % NOTES
-% The package directory must be on the path
+% The package directory must in the environment path
+% (MATLAB) Omit the '+' prefix when identifying package names
 function obj=unwrapComponent(pkg,varargin)
   obj=feval([pkg,'.',pkg],varargin{:});
 end
