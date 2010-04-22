40eedcea31fdc40a51fc10e25e36a3f5a2fbf64a
dddvision <dddvision@gmail.com>
2010-04-22 21:21:55 +0000

    Beginning to add BrownianPlanarDiscrete component (in C++). Updated comments and spacing in a few other places.

--- components/+OpticalFlowOpenCV/@OpticalFlowOpenCV/private/Quat2Euler.m
+++ components/+OpticalFlowOpenCV/@OpticalFlowOpenCV/private/Quat2Euler.m
@@ -5,8 +5,6 @@
 %
 % OUTPUT
 % E = Euler angles, in the order forward-right-down (4-by-N)
-
-
 function E=Quat2Euler(Q)
 
 N=size(Q,2);
@@ -29,16 +27,9 @@ q14=q1.*q4;
 q13=q1.*q3;
 q24=q2.*q4;
 
-if isnumeric(Q)
-  E=zeros(3,N);
-  E(1,:)=atan2(2*(q34+q12),q11-q22-q33+q44);
-  E(2,:)=real(asin(-2*(q24-q13)));
-  E(3,:)=atan2(2*(q23+q14),q11+q22-q33-q44);
-else
-  E=sym(zeros(3,N));
-  E(1,:)=atan((2*(q34+q12))./(q11-q22-q33+q44));
-  E(2,:)=asin(-2*(q24-q13));
-  E(3,:)=atan((2*(q23+q14))./(q11+q22-q33-q44));
-end
+E=zeros(3,N);
+E(1,:)=atan2(2*(q34+q12),q11-q22-q33+q44);
+E(2,:)=real(asin(-2*(q24-q13)));
+E(3,:)=atan2(2*(q23+q14),q11+q22-q33-q44);
 
 end
--- components/+OpticalFlowOpenCV/@OpticalFlowOpenCV/private/computeIntermediateData.m
+++ components/+OpticalFlowOpenCV/@OpticalFlowOpenCV/private/computeIntermediateData.m
@@ -31,38 +31,3 @@ function data=computeIntermediateData(this,ka,kb)
   end
   
 end
-  
-% add 1 to indicies for MATLAB representation
-% pixA = pixA+1;
-% pixB = pixB+1;
-% 
-% figure(1); clf;
-% if this.isDense
-%     u = pixA(:,1) - pixB(:,1);
-%     v = pixA(:,2) - pixB(:,2);
-%     u = reshape(u,size(I1));
-%     v = reshape(v,size(I1));
-%     
-%     % remove points with big OF
-%     THRES = 40;
-%     ind = find(u>THRES | u<-THRES);
-%     u(ind) = NaN;
-%     ind = find(v>THRES | v<-THRES);
-%     v(ind) = NaN;
-%     SAMPLE_SIZE = 10;
-%     SCALE = 5;
-%     im(I1(1:SAMPLE_SIZE:end,1:SAMPLE_SIZE:end),[],0);
-%     hold('on');
-%     quiver( u(1:SAMPLE_SIZE:end,1:SAMPLE_SIZE:end),...
-%         v(1:SAMPLE_SIZE:end,1:SAMPLE_SIZE:end), SCALE,'-b'); hold('off');
-% 
-% else
-%     % plot results
-%     colormap('gray');
-%     imagesc(I1);
-%     hold on;
-%     for ind = 1: size(pixA,1)
-%         line([pixA(ind,1) pixB(ind,1)], [pixA(ind,2) pixB(ind,2)], 'Color', 'c');
-%     end
-%     hold off;
-% end
