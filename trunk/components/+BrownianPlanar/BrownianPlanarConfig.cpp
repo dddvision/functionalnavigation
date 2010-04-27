c2e745ffde0926cf64cdb0f260ae7788d114861e
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-04-27 07:56:59 +0000

    More updates to the BrownianPlanar component...almost done.

---
+++ trunk/components/+BrownianPlanar/BrownianPlanarConfig.cpp
@@ -0,0 +1,8 @@
+namespace tommas
+{
+  const double BrownianPlanar::rate=0.5;
+  const double BrownianPlanar::initialPosition[3]={0,0,0};
+  const double BrownianPlanar::initialQuaternion[4]={1,0,0,0};
+  const double BrownianPlanar::normalizedMass=0.5;
+  const double BrownianPlanar::normalizedRotationalMass=2;
+}
--- trunk/components/+BrownianPlanarDiscrete/BrownianPlanarDiscreteConfig.cpp
+++
@@ -1,6 +0,0 @@
-namespace tommas
-{
-  const double BrownianPlanarDiscrete::rate=0.5;
-  const double BrownianPlanarDiscrete::initialPosition[3]={0,0,0};
-  const double BrownianPlanarDiscrete::initialQuaternion[4]={1,0,0,0};
-}
