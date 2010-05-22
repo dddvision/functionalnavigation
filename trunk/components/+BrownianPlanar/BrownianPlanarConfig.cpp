e6b81e40f845c44ea1c34a3b6836d95ac90a3e2d
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-05-22 03:03:46 +0000

    Major updates to DynamicModelWrapper and other functions related to the C++ class BrownianPlanar.

--- trunk/components/+BrownianPlanar/BrownianPlanarConfig.cpp
+++ trunk/components/+BrownianPlanar/BrownianPlanarConfig.cpp
@@ -1,6 +1,6 @@
 namespace tommas
 {
-  const double BrownianPlanar::rate=0.5;
+  const double BrownianPlanar::rate=2.0;
   const double BrownianPlanar::initialPosition[3]={0,0,0};
   const double BrownianPlanar::initialQuaternion[4]={1,0,0,0};
   const double BrownianPlanar::normalizedMass=0.5;
