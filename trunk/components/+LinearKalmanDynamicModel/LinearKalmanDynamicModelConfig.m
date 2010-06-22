90d2a30cc86d5a42fa127b3fa41480da43509ef8
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-06-22 19:31:08 +0000

    Removing the LinearKalmanDynamicModel that was replaced by XDynamics...again.

--- trunk/components/+LinearKalmanDynamicModel/LinearKalmanDynamicModelConfig.m
+++
@@ -1,8 +0,0 @@
-classdef LinearKalmanDynamicModelConfig < handle
-  properties (Constant=true)
-    positionOffset=-1.2; % simulated offset in first position coordinate
-    positionRateOffset=0.2; % simulated offset in first position rate coordinate
-    positionDeviation=2; % standard deviation of initial position distribution
-    positionRateDeviation=0.1; % standard deviation of initial position rate distribution
-  end
-end
