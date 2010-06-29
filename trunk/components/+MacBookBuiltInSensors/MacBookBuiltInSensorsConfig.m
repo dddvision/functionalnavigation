5a935dfe340a19791167315ae0ecdb8097c54d9a
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-06-29 03:57:48 +0000

    Minor updates related to the new MacBookBuiltInSensors class. The camera is ready, but the inertial sensor is not.

--- trunk/components/+MacBookBuiltInSensors/MacBookBuiltInSensorsConfig.m
+++ trunk/components/+MacBookBuiltInSensors/MacBookBuiltInSensorsConfig.m
@@ -1,9 +1,8 @@
 classdef MacBookBuiltInSensorsConfig < handle
   properties (Constant=true)
-    frameIncrement=2; % (1) number of frames to advance per data node
-    overwrite=true; % (true) overwrite cached data, or use previously recorded data
-    %rate=0.073502; % (0.073502) seconds per frame
-    frameOffset=[0;0;0;1;0;0;0]; % TODO: measure offset of camera frame
-    timeOut=5; % (5) seconds to wait for camera to initialize
+    cameraIncrement=10; % (1) number of frames to advance per data node
+    % TODO: measure offset of camera frame
+    cameraFrameOffset=[0;0;0;1;0;0;0]; % offset of the camera frame relative to the inertial sensor
+    timeOut=10; % (10) seconds to wait for any stage of sensor initialization
   end
 end
