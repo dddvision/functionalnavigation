862b9b9b90e9dc50b48d35b56ab264d32415ec6c
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-07-19 17:02:36 +0000

    Fixed: Removed the cause of truncation warning in demo.m. Also removed the inclusion of the DataContainer class in tommas.h, which allows BrownianPlanar to compile.

--- trunk/tommas.h
+++ trunk/tommas.h
@@ -1,14 +1,12 @@
 #ifndef TOMMAS_H
 #define TOMMAS_H
 
-#include "DataContainer.h"
 #include "DynamicModel.h"
 #include "Measure.h"
 #include "Optimizer.h"
 
 namespace tommas
 {
-  std::map<std::string,DataContainerFactory> dataContainerList;
   std::map<const std::string,DynamicModelFactory> dynamicModelList;
   std::map<std::string,MeasureFactory> measureList;
   std::map<const std::string,OptimizerFactory> optimizerList;
