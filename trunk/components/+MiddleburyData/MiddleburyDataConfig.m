86a54a0d3172146843e1092987b8dcf4cd12a07c
oreifej <oreifej@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-07-21 23:56:22 +0000

    Changing the default size to HalfSize

--- trunk/components/+MiddleburyData/MiddleburyDataConfig.m
+++ trunk/components/+MiddleburyData/MiddleburyDataConfig.m
@@ -1,7 +1,7 @@
 classdef MiddleburyDataConfig  < handle
   properties (Constant=true,GetAccess=protected)
     sceneYear = 'scenes2005'; % 'scenes2005', 'scenes2006'
-    fractionalSize = 'ThirdSize'; % 'FullSize', 'HalfSize', 'ThirdSize'   
+    fractionalSize = 'HalfSize'; % 'FullSize', 'HalfSize', 'ThirdSize'   
     scene = 'Books'; % 'Art', 'Books', 'Aloe', ...}
     illumination = 'Illum2'; % 'Illum1', 'Illum2', 'Illum3'
     exposure = 'Exp1'; % 'Exp0', 'Exp1', 'Exp2'
