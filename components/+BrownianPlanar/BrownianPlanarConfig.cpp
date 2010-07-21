b7235f728f9e7dd8b52192f9b111a48a1e58a1d7
dddvision <dddvision@gmail.com>
2010-07-21 05:29:56 +0000

    Incorporated initial conditions into BrownianPlanar class. Reverted DemoConfig to default state.

--- components/+BrownianPlanar/BrownianPlanarConfig.cpp
+++ components/+BrownianPlanar/BrownianPlanarConfig.cpp
@@ -1,8 +1,8 @@
 namespace tommas
 {
-  const double BrownianPlanar::rate=2.0;
-  const double BrownianPlanar::initialPosition[3]={0,0,0};
-  const double BrownianPlanar::initialQuaternion[4]={1,0,0,0};
-  const double BrownianPlanar::normalizedMass=0.5;
-  const double BrownianPlanar::normalizedRotationalMass=2;
+  const double BrownianPlanar::rate=5.0; // strictly positive
+  const double BrownianPlanar::initialPosition[3]={0,0,0}; // initial position in ECEF
+  const double BrownianPlanar::initialQuaternion[4]={1,0,0,0}; // unit magnitude to machine precision
+  const double BrownianPlanar::normalizedMass=0.5; // strictly positive
+  const double BrownianPlanar::normalizedRotationalMass=100; // strictly positive
 }
