5f1df4525f62cd255a4ecce4b3fc55c7571a3028
dddvision <dddvision@gmail.com>
2010-07-23 13:15:10 +0000

    Added silent mode for XMeasure.

--- components/+XMeasure/XMeasureConfig.m
+++ components/+XMeasure/XMeasureConfig.m
@@ -1,6 +1,7 @@
 classdef XMeasureConfig < handle
   properties (Constant=true)
-    dt=0.01; % fixed time step
-    deviation=0.01; % deviation of measurement noise
+    dt=0.01; % (0.01) fixed time step
+    deviation=0.01; % (0.01) deviation of measurement noise
+    verbose=false; % (false) print messages to stdandard output
   end
 end
