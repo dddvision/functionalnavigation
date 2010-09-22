65c7b9825229a8532193e569d9dfb09463a03785
dddvision <dddvision@gmail.com>
2010-09-22 17:10:25 +0000

    Added demonstration of TrackFeaturesKLT. To try it out, run addpath(components); tom.Measure.create(FastPBM,matlab:MiddleburyData);

--- components/+FastPBM/@FastPBM/private/TrackFeaturesKLT.m
+++ components/+FastPBM/@FastPBM/private/TrackFeaturesKLT.m
@@ -1,11 +1,13 @@
-% ia = sub-pixel rows of patch centers in first frame
-% ib = sub-pixel columns of patch centers in first frame
-% Pin = pyramid structure (created by BuildPyramid()
-% y = image in range [0,1]
-% ia = sub-pixel rows of patch centers in first frame
-% ib = sub-pixel columns of patch centers in first frame
-% Pin = pyramid structure (created by BuildPyramid()
-
+% Finds locations of features in image B based on their locations in imageA
+%
+% @param[in] ia  sub-pixel rows of patch centers in first frame
+% @param[in] ja  sub-pixel columns of patch centers in first frame
+% @param[in] Pin pyramid structure (created by BuildPyramid()
+% @param[in] y   image in range [0,1]
+%
+% @param[out] ibe  sub-pixel rows of patch centers in first frame
+% @param[out] jbe  sub-pixel columns of patch centers in first frame
+% @param[out] Pout pyramid structure (created by BuildPyramid()
 function [ibe,jbe,Pout]=TrackFeaturesKLT(ia,ja,Pin,y,halfwin,RESIDUE_THRESH)
 
   LEVELS=length(Pin);
@@ -23,7 +25,7 @@ function [ibe,jbe,Pout]=TrackFeaturesKLT(ia,ja,Pin,y,halfwin,RESIDUE_THRESH)
     gib=Pout{L}.gi;
     gjb=Pout{L}.gj;
 
-    [ibe,jbe]=MEXtrackFeaturesKLT(za,gia,gja,ia,ja,zb,gib,gjb,ibe,jbe,L,2*halfwin+1,RESIDUE_THRESH);
+    [ibe,jbe]=mexTrackFeaturesKLT(za,gia,gja,ia,ja,zb,gib,gjb,ibe,jbe,L,2*halfwin+1,RESIDUE_THRESH);
   end
 
 end
