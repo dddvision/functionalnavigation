1e451e16c51fa25fb8b6e704705c17d90ac71135
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-09-23 20:15:09 +0000

    Beginning to chain together multiple images in FastPBM/SparseTrackerKLT, though the tracker has not been completely implemented.

--- trunk/components/+FastPBM/SparseTracker.m
+++ trunk/components/+FastPBM/SparseTracker.m
@@ -4,13 +4,7 @@
 % n=first(this);
 % n=last(this);
 % time=getTime(this,n);
-classdef SparseTracker < Sensor
-
-  properties (Constant=true,GetAccess=public)
-    maxFeatures = 30; % (30) maximum integer number of features per frame to find
-    maxFrames = 10; % (10) maximum integer number of frames to track a feature
-    maxSearch = 0.05; % (0.05) approximate maximum angle to search for a feature in radians
-  end
+classdef SparseTracker < tom.Sensor
 
   methods (Access=public,Static=true)
     % Constructor
