8294c461e62997ca8e475454dda8c1aab544b487
dddvision <dddvision@gmail.com>
2010-09-24 18:56:39 +0000

    Adjusted intrinsic parameters in MiddleburyData to match http://vision.middlebury.edu/stereo/data/scenes2005/

--- components/+MiddleburyData/BodyReference.m
+++ components/+MiddleburyData/BodyReference.m
@@ -1,9 +1,13 @@
 classdef BodyReference < MiddleburyData.MiddleburyDataConfig & tom.Trajectory
-  
-  properties (SetAccess=private,GetAccess=private)
+
+  properties (Constant=true,GetAccess=private)
+    speed = 0.160; % Reference: http://vision.middlebury.edu/stereo/data/scenes2005/
+  end
+
+  properties (Access=private)
     interval
   end  
-  
+
   methods (Access=public)
     function this=BodyReference
       this.interval=tom.TimeInterval(tom.WorldTime(0),tom.WorldTime((this.numImages-1)/this.fps));
@@ -20,11 +24,11 @@ classdef BodyReference < MiddleburyData.MiddleburyDataConfig & tom.Trajectory
       tmax=double(this.interval.second);
       good=(t>=tmin)&(t<=tmax);
       for k=find(good)
-        pose(k).p=[0;t(k);0];
+        pose(k).p=[0;this.speed*t(k);0];
         pose(k).q=[1;0;0;0];
       end
     end
-    
+
     function tangentPose=tangent(this,t)
       tangentPose(1,numel(t))=tom.TangentPose;
       t=double(t);
@@ -32,12 +36,12 @@ classdef BodyReference < MiddleburyData.MiddleburyDataConfig & tom.Trajectory
       tmax=double(this.interval.second);
       good=(t>=tmin)&(t<=tmax);
       for k=find(good)
-        tangentPose(k).p=[0;t(k);0];
+        tangentPose(k).p=[0;this.speed*t(k);0];
         tangentPose(k).q=[1;0;0;0];
-        tangentPose(k).r=[0;1;0];
+        tangentPose(k).r=[0;this.speed;0];
         tangentPose(k).s=[0;0;0;0];
       end
     end
   end
-  
+
 end
