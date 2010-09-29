ec5c845836566760f05ee89da526c20ec29f8c8d
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-09-29 23:02:24 +0000

    Minor bug fix in DemoDisplay.

--- trunk/DemoConfig.m
+++ trunk/DemoConfig.m
@@ -6,7 +6,6 @@ classdef DemoConfig < handle
     dynamicModelName = 'Default'; % ('Default') name of a single DynamicModel component
     measureNames = {'Default'}; % ({'Default'}) cell array of zero or more names of Measure component
     optimizerName = 'Default'; % ('Default') name of a single Optimizer component
-    defaultInitialTime = tom.WorldTime(0); % (tom.WorldTime(0)) default initial time when no data is available
     numSteps = uint32(1E9); % (uint32(1E9)) number of optimization steps until termination
     
     % display parameters
@@ -19,7 +18,7 @@ classdef DemoConfig < handle
     scale = 0.01; % (0.01) scale of body frame
     bigSteps = 10; % (10) number of body frames per trajectory
     subSteps = 10; % (10) number of line segments between body frames
-    infinity = 100; % (100) replaces infinity as a time domain upper bound
+    infinity = 1000; % (1000) maximum span of time domain if upper bound is infinite
     colorBackground = [1,1,1]; % ([1,1,1]) color of figure background
     colorHighlight = [1,0,0]; % ([1,0,0]) color of objects to emphasize
     colorReference = [0,1,0]; % ([0,1,0]) color of reference objects
