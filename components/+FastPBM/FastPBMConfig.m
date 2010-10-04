af8058286be59b6eed010b2860d53383cc8c9c40
oreifej <oreifej@gmail.com>
2010-10-04 15:01:38 +0000

    Initial changes to include the tracking measure ....(It does not run now, will be running by tom 10/5)

--- components/+FastPBM/FastPBMConfig.m
+++ components/+FastPBM/FastPBMConfig.m
@@ -3,5 +3,7 @@ classdef FastPBMConfig < handle
     maxFeatures = 200; % (200) maximum integer number of features per frame to find
     maxSearch = 0.02; % (0.02) approximate maximum angle to search for a feature in radianss
     displayFeatures = true; % (false) show tracked features
+    
+    maxCost = 20; % (20) maximum cost for a single graph edge
   end
 end
