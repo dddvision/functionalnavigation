408cc4da32d5e192e60a835110f8bfd662081137
dddvision <dddvision@gmail.com>
2010-10-06 02:16:01 +0000

    Changed default display of OpticalFlowOpenCV to true and improved figure handling.

--- components/+OpticalFlowOpenCV/OpticalFlowOpenCVConfig.m
+++ components/+OpticalFlowOpenCV/OpticalFlowOpenCVConfig.m
@@ -1,6 +1,6 @@
 classdef OpticalFlowOpenCVConfig < handle
   properties (Constant=true,GetAccess=protected)
-    displayFlow = false; % (false) display flow results
+    displayFlow = true; % (true) display flow results
     isDense = false; % (false) compute flow over all pixels
     windowSize = 9; % (9) search window at each level
     levels = 5; % (5) number of pyramid levels to use
