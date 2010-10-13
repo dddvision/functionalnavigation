19abcb1621ffe957c93fb4f3b058ade444b47904
dddvision <dddvision@gmail.com>
2010-10-13 22:16:00 +0000

    PATRICK: Code in TrajectoryPerturbation has been made more concise by invoking Quat2Homo. In other news, moved a few more TODO.txt items to the online issue tracker.

--- +testbed/TrajectoryPerturbation.m
+++ +testbed/TrajectoryPerturbation.m
@@ -1,51 +1,43 @@
 classdef TrajectoryPerturbation < Trajectory
 
-  properties (GetAccess=private,SetAccess=private)
-      %the change in 
+  properties (GetAccess = private, SetAccess = private)
       translation
       rotation
       deltaTranslation
       deltaRotation
   end
     
-  methods (Access=public)
+  methods (Access = public, Static = true)
     % foward,right,down   -   quat
     % base trajectory and rotation (ie ground truth)
     function this = TrajectoryPerturbation(trans, rot)
       this.translation = trans;
       this.rotation = rot;
     end
+  end
     
-    function setPerturbation(deltaTrans,deltaRot)
+  methods (Access = public, Static = false)
+    function setPerturbation(this, deltaTrans, deltaRot)
       this.deltaTranslation = deltaTrans;
       this.deltaRotation = deltaRot;
     end
     
-    
-    function pose=evaluate(this,t)
-      finalQuat = zeroes(4);
-      finalQuat(1) =  this.deltaRotation(1)*this.rotation(1) ...
-                    - this.deltaRotation(2)*this.rotation(2) ...
-                    - this.deltaRotation(3)*this.rotation(3) ...
-                    - this.deltaRotation(4)*this.rotation(4);
-      finalQuat(2) =  this.deltaRotation(2)*this.rotation(1) ...
-                    + this.deltaRotation(1)*this.rotation(2) ...
-                    + this.deltaRotation(3)*this.rotation(4) ...
-                    - this.deltaRotation(4)*this.rotation(3);
-      finalQuat(3) =  this.deltaRotation(1)*this.rotation(3) ...
-                    - this.deltaRotation(2)*this.rotation(4) ...
-                    + this.deltaRotation(3)*this.rotation(1) ...
-                    + this.deltaRotation(4)*this.rotation(2);
-      finalQuat(4) =  this.deltaRotation(1)*this.rotation(4) ...
-                    + this.deltaRotation(2)*this.rotation(3) ...
-                    - this.deltaRotation(3)*this.rotation(2) ...
-                    + this.deltaRotation(4)*this.rotation(1);
-
-      newPose = struct(  'p', this.translation+this.deltaTranslation, ...
-                         'q', finalQuat);
-      pose = Pose(newPose);
-      
+    function pose = evaluate(this, t)
+      pFinal = this.translation+this.deltaTranslation;
+      qFinal = Quat2Homo(this.deltaRotation)*this.rotation;
+      pose = tom.Pose(struct('p', pFinal, 'q', qFinal));      
     end
   end
   
+end
+
+function h = Quat2Homo(q)
+  q1 = q(1);
+  q2 = q(2);
+  q3 = q(3);
+  q4 = q(4);
+  h = [[q1, -q2, -q3, -q4]
+       [q2,  q1, -q4,  q3]
+       [q3,  q4,  q1, -q2]
+       [q4, -q3,  q2,  q1]];
 end
