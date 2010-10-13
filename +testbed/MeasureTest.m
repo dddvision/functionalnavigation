5ec0a53c45fd4bcc22fcb4a685cc27a7d6f102ab
dddvision <dddvision@gmail.com>
2010-10-13 18:55:16 +0000

    Moved a few comments from MeasureTest to the online issue tracker.

--- +testbed/MeasureTest.m
+++ +testbed/MeasureTest.m
@@ -85,19 +85,6 @@ classdef MeasureTest < handle
           end
         end
       end
-      
-      % For each edge that the measure supports for the specified data set
-        % Compute bias? (distance from ground truth to cost minimum)
-        % Perturb trajectory small amounts around ground truth and evaluate costs
-        % Include tests with ta=tb and ta~=tb
-        % Granularity (dilution of precision, distance until cost increases at all)
-        % Monotonicity (distance until cost begins to decrease)
-        % Cost (maybe smoothed around ground truth)
-        % Jacobian (sensitivity of cost to pertrubation from ground truth)
-        % Hessian (eigenvalues and consistency of their ratios, eigenvectors)
-        % Time to run findEdges
-        % Time to run evaluateEdgeCost initially
-        % Time to run evaluateEdgeCost repeated
         
       fprintf('\n\n*** End Measure Test ***');
     end
