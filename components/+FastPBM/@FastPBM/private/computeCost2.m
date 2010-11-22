c67b5853b8f041f0c591c3c251de4bbefbfeb85b
oreifej <oreifej@gmail.com>
2010-11-22 20:43:42 +0000

    Computing cost based on the tracking error model ..

--- components/+FastPBM/@FastPBM/private/computeCost2.m
+++ components/+FastPBM/@FastPBM/private/computeCost2.m
@@ -1,22 +1,25 @@
- function cost=computeCost2(this, model,T,rayA,rayB)
+function cost=computeCost2(this, model,T,rayA,rayB)
 % Calculate the error for each ray-pair
 N = zeros(3,size(rayA,2));
 Errors = zeros(1,size(rayB,2));
 for indVec=1:size(rayA,2)
-
+    
     % calculate the normal to the epipolar plane
     N(:,indVec) = cross(T,rayA(:,indVec));
     
     % normalize vectors
     n = N(:,indVec)./norm(N(:,indVec));
-    x = rayB(:,indVec)/norm(rayB(:,indVec)); 
+    x = rayB(:,indVec)/norm(rayB(:,indVec));
     
-    % calculate the error 
+    % calculate the error
     Errors(indVec) = n'*x;
 end
 
 % load the model and use that to get the cost
 M = model.parameters(1);
 S = model.parameters(2);
+YMax = normpdf(M,M,S);
 Y = normpdf(Errors,M,S);
-cost = sum(Y);
+Y = Y./YMax;
+cost = sum(-log(Y))./length(Y);
+
