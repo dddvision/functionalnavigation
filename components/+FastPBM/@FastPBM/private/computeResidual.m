bfa1ddfe6e379ec9c5c94a880c8e7cbc3d16c825
oreifej <oreifej@gmail.com>
2010-12-01 22:08:28 +0000

    Handling the zero translation case

--- components/+FastPBM/@FastPBM/private/computeResidual.m
+++ components/+FastPBM/@FastPBM/private/computeResidual.m
@@ -1,20 +1,19 @@
 function residual = computeResidual(translation, rayA, rayB)
-  numRays = size(rayA, 2);
-  normal = zeros(3, numRays);
-  residual = zeros(1, numRays);
-  
-  for index = 1:numRays
-    % calculate the normal to the epipolar plane
-    normal(:, index) = cross(translation, rayA(:, index));
 
-    % normalize vectors
-    n = normal(:, index)/norm(normal(:, index));
-    % TODO: set the residual to zero when the absolute value of the denominator is less than eps 
+if  norm(translation) < eps
+    residual = acos(dot(rayA,rayB));
+else
+    % calculate the normal to the epipolar plane
+    normals = cross(repmat(translation,1,size(rayA,2)),rayA);
+    
+    % normalize the normals
+    nNormals = normals./repmat(sqrt(sum(normals.^2)),3,1);
+    % TODO: set the residual to zero when the absolute value of the
+    % denominator is less than eps
     
     % calculate the error
-    residual(index) = dot(n, rayB(:, index));
+    residual = dot(nNormals,rayB);
     % TODO: Consider taking the acos of the dot product to get angular error in radians.
     %       This change is debatable because it affects the shape of the distribution.
     % NOTE: rayB is guaranteed to have unit magnitude
-  end
 end
