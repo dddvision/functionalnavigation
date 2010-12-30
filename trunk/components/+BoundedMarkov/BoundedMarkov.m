1bbfe7fa49539833dd208512850161fa7ddcd141
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2010-12-30 22:04:00 +0000

    Updated code formatting in BoundedMarkov (so that code copied from it will meet the formatting spec)

--- trunk/components/+BoundedMarkov/BoundedMarkov.m
+++ trunk/components/+BoundedMarkov/BoundedMarkov.m
@@ -1,16 +1,16 @@
 classdef BoundedMarkov < BoundedMarkov.BoundedMarkovConfig & tom.DynamicModel
   
-  properties (Constant=true,GetAccess=private)
-    initialNumLogical=uint32(0);
-    initialNumUint32=uint32(0);
-    extensionNumLogical=uint32(0);
-    initialBlockCost=0;
-    extensionBlockCost=0;
-    chunkSize=256;
-    numStates=12;
+  properties (Constant = true, GetAccess = private)
+    initialNumLogical = uint32(0);
+    initialNumUint32 = uint32(0);
+    extensionNumLogical = uint32(0);
+    initialBlockCost = 0;
+    extensionBlockCost = 0;
+    chunkSize = 256;
+    numStates = 12;
   end
   
-  properties (GetAccess=private,SetAccess=private)
+  properties (GetAccess = private, SetAccess = private)
     interval
     numInputs
     initialBlock
@@ -22,211 +22,211 @@ classdef BoundedMarkov < BoundedMarkov.BoundedMarkovConfig & tom.DynamicModel
     ABZ % intermediate formulation of A and B matrices with zeros appended
   end
   
-  methods (Static=true,Access=public)
+  methods (Static = true, Access = public)
     function initialize(name)
-      function text=componentDescription
-        text='Represents the integration of linear Markov motion model with a bounded forcing function.';
+      function text = componentDescription
+        text = 'Represents the integration of linear Markov motion model with a bounded forcing function.';
       end
-      tom.DynamicModel.connect(name,@componentDescription,@BoundedMarkov.BoundedMarkov);
+      tom.DynamicModel.connect(name, @componentDescription, @BoundedMarkov.BoundedMarkov);
     end
   end
   
-  methods (Access=public)
-    function this=BoundedMarkov(initialTime,uri)
-      this=this@tom.DynamicModel(initialTime,uri);
-      this.initialBlock=struct('logical',false(1,this.initialNumLogical),...
-        'uint32',zeros(1,this.initialNumUint32,'uint32'));
-      this.firstNewBlock=1;
-      this.interval=tom.TimeInterval(initialTime,initialTime);
-      this.block=struct('logical',{},'uint32',{});
-      this.numInputs=size(this.B,2);
-      this.state=zeros(this.numStates,this.chunkSize);
-      this.ABZ=[this.A,this.B;sparse(this.numInputs,this.numStates+this.numInputs)];
-      ABd=expmApprox(this.ABZ/this.rate);
-      this.Ad=sparse(ABd(1:this.numStates,1:this.numStates));
-      this.Bd=sparse(ABd(1:this.numStates,(this.numStates+1):end));
+  methods (Access = public)
+    function this = BoundedMarkov(initialTime, uri)
+      this = this@tom.DynamicModel(initialTime, uri);
+      this.initialBlock = struct('logical', false(1, this.initialNumLogical), ...
+        'uint32', zeros(1, this.initialNumUint32, 'uint32'));
+      this.firstNewBlock = 1;
+      this.interval = tom.TimeInterval(initialTime, initialTime);
+      this.block = struct('logical', {}, 'uint32', {});
+      this.numInputs = size(this.B, 2);
+      this.state = zeros(this.numStates, this.chunkSize);
+      this.ABZ = [this.A, this.B;sparse(this.numInputs, this.numStates+this.numInputs)];
+      ABd = expmApprox(this.ABZ/this.rate);
+      this.Ad = sparse(ABd(1:this.numStates, 1:this.numStates));
+      this.Bd = sparse(ABd(1:this.numStates, (this.numStates+1):end));
     end
 
-    function interval=domain(this)
-      interval=this.interval;
+    function interval = domain(this)
+      interval = this.interval;
     end
    
-    function pose=evaluate(this,t)
-      N=numel(t);
+    function pose = evaluate(this, t)
+      N = numel(t);
       if(N==0)
-        pose=repmat(tom.Pose,[1,0]);
+        pose = repmat(tom.Pose, [1, 0]);
       else
-        [lowerBound,upperBound,dkFloor,dtRemain]=preEvaluate(this,t);
-        pose(1,N)=tom.Pose;
-        for n=find(lowerBound)
+        [lowerBound, upperBound, dkFloor, dtRemain] = preEvaluate(this, t);
+        pose(1, N) = tom.Pose;
+        for n = find(lowerBound)
           if(upperBound(n))
-            substate=subIntegrate(this,dkFloor(n),dtRemain(n));
-            pose(n).p=substate(1:3)+this.initialPosition;
-            pose(n).q=Quat2Homo(AxisAngle2Quat(substate(4:6)))*this.initialRotation; % verified
+            substate = subIntegrate(this, dkFloor(n), dtRemain(n));
+            pose(n).p = substate(1:3)+this.initialPosition;
+            pose(n).q = Quat2Homo(AxisAngle2Quat(substate(4:6)))*this.initialRotation; % verified
           else
-            finalTangentPose=tangent(this,this.interval.second);
-            pose(n)=predictPose(finalTangentPose,t(n)-this.interval.second);
+            finalTangentPose = tangent(this, this.interval.second);
+            pose(n) = predictPose(finalTangentPose, t(n)-this.interval.second);
           end
         end
       end
     end
     
-    function tangentPose=tangent(this,t)
-      N=numel(t);
+    function tangentPose = tangent(this, t)
+      N = numel(t);
       if(N==0)
-        tangentPose=repmat(tom.TangentPose,[1,0]);
+        tangentPose = repmat(tom.TangentPose, [1, 0]);
       else
-        [lowerBound,upperBound,dkFloor,dtRemain]=preEvaluate(this,t);
-        tangentPose(1,N)=tom.TangentPose;
-        for n=find(lowerBound)
+        [lowerBound, upperBound, dkFloor, dtRemain] = preEvaluate(this, t);
+        tangentPose(1, N) = tom.TangentPose;
+        for n = find(lowerBound)
           if(upperBound(n))
-            substate=subIntegrate(this,dkFloor(n),dtRemain(n));
-            tangentPose(n).p=substate(1:3)+this.initialPosition;
-            tangentPose(n).q=Quat2Homo(AxisAngle2Quat(substate(4:6)))*this.initialRotation; % verified
-            tangentPose(n).r=substate(7:9)+this.initialPositionRate;
-            tangentPose(n).s=this.initialOmega+substate(10:12);
+            substate = subIntegrate(this, dkFloor(n), dtRemain(n));
+            tangentPose(n).p = substate(1:3)+this.initialPosition;
+            tangentPose(n).q = Quat2Homo(AxisAngle2Quat(substate(4:6)))*this.initialRotation; % verified
+            tangentPose(n).r = substate(7:9)+this.initialPositionRate;
+            tangentPose(n).s = this.initialOmega+substate(10:12);
           else
-            finalTangentPose=tangent(this,this.interval.second);
-            tangentPose(n)=predictTangentPose(finalTangentPose,t(n)-this.interval.second);
+            finalTangentPose = tangent(this, this.interval.second);
+            tangentPose(n) = predictTangentPose(finalTangentPose, t(n)-this.interval.second);
           end
         end
       end
     end
     
-    function num=numInitialLogical(this)
-      num=this.initialNumLogical;
+    function num = numInitialLogical(this)
+      num = this.initialNumLogical;
     end
     
-    function num=numInitialUint32(this)
-      num=this.initialNumUint32;      
+    function num = numInitialUint32(this)
+      num = this.initialNumUint32;      
     end
     
-    function num=numExtensionLogical(this)
-      num=this.extensionNumLogical;
+    function num = numExtensionLogical(this)
+      num = this.extensionNumLogical;
     end
     
-    function num=numExtensionUint32(this)
-      num=uint32(size(this.B,2));
+    function num = numExtensionUint32(this)
+      num = uint32(size(this.B, 2));
     end
     
-    function num=numExtensionBlocks(this)
-      num=uint32(numel(this.block));
+    function num = numExtensionBlocks(this)
+      num = uint32(numel(this.block));
     end
     
-    function v=getInitialLogical(this,p)
-      v=this.initialBlock.logical(p+1);
+    function v = getInitialLogical(this, p)
+      v = this.initialBlock.logical(p+1);
     end
     
-    function v=getInitialUint32(this,p)
-      v=this.initialBlock.uint32(p+1);
+    function v = getInitialUint32(this, p)
+      v = this.initialBlock.uint32(p+1);
     end
     
-    function v=getExtensionLogical(this,b,p)
-      v=this.block(b+1).logical(p+1);
+    function v = getExtensionLogical(this, b, p)
+      v = this.block(b+1).logical(p+1);
     end
     
-    function v=getExtensionUint32(this,b,p)
-      v=this.block(b+1).uint32(p+1);
+    function v = getExtensionUint32(this, b, p)
+      v = this.block(b+1).uint32(p+1);
     end
     
-    function setInitialLogical(this,p,v)
-      this.initialBlock.logical(p+1)=v;
+    function setInitialLogical(this, p, v)
+      this.initialBlock.logical(p+1) = v;
     end
     
-    function setInitialUint32(this,p,v)
-      this.initialBlock.uint32(p+1)=v;
+    function setInitialUint32(this, p, v)
+      this.initialBlock.uint32(p+1) = v;
     end
     
-    function setExtensionLogical(this,b,p,v)
-      this.block(b+1).logical(p+1)=v;
-      this.firstNewBlock=min(this.firstNewBlock,b+1);
+    function setExtensionLogical(this, b, p, v)
+      this.block(b+1).logical(p+1) = v;
+      this.firstNewBlock = min(this.firstNewBlock, b+1);
     end
     
-    function setExtensionUint32(this,b,p,v)
-      this.block(b+1).uint32(p+1)=v;
-      this.firstNewBlock=min(this.firstNewBlock,b+1);
+    function setExtensionUint32(this, b, p, v)
+      this.block(b+1).uint32(p+1) = v;
+      this.firstNewBlock = min(this.firstNewBlock, b+1);
     end
 
-    function cost=computeInitialBlockCost(this)
-      cost=this.initialBlockCost;
+    function cost = computeInitialBlockCost(this)
+      cost = this.initialBlockCost;
     end
 
-    function cost=computeExtensionBlockCost(this,b)
-      assert(isa(b,'uint32'));
+    function cost = computeExtensionBlockCost(this, b)
+      assert(isa(b, 'uint32'));
       assert(numel(b)==1);
-      cost=this.extensionBlockCost;
+      cost = this.extensionBlockCost;
     end
     
     function extend(this)
-      blank=struct('logical',false(0,1),'uint32',zeros(1,numExtensionUint32(this),'uint32'));
-      this.block=cat(2,this.block,blank);
-      N=numel(this.block);
-      if((N+1)>size(this.state,2))
-        this.state=[this.state,zeros(this.numStates,this.chunkSize)];
+      blank = struct('logical', false(0, 1), 'uint32', zeros(1, numExtensionUint32(this), 'uint32'));
+      this.block = cat(2, this.block, blank);
+      N = numel(this.block);
+      if((N+1)>size(this.state, 2))
+        this.state = [this.state, zeros(this.numStates, this.chunkSize)];
       end
-      this.interval.second=this.interval.first+N/this.rate;
+      this.interval.second = this.interval.first+N/this.rate;
     end
      
-    function obj=copy(this)
-      obj=BoundedMarkov.BoundedMarkov(tom.WorldTime(0),'');
-      obj.interval=this.interval;
-      obj.numInputs=this.numInputs;
-      obj.initialBlock=this.initialBlock;
-      obj.firstNewBlock=this.firstNewBlock;
-      obj.block=this.block;
-      obj.state=this.state;
-      obj.Ad=this.Ad;
-      obj.Bd=this.Bd;
-      obj.ABZ=this.ABZ;
+    function obj = copy(this)
+      obj = BoundedMarkov.BoundedMarkov(tom.WorldTime(0), '');
+      obj.interval = this.interval;
+      obj.numInputs = this.numInputs;
+      obj.initialBlock = this.initialBlock;
+      obj.firstNewBlock = this.firstNewBlock;
+      obj.block = this.block;
+      obj.state = this.state;
+      obj.Ad = this.Ad;
+      obj.Bd = this.Bd;
+      obj.ABZ = this.ABZ;
     end
   end
   
-  methods (Access=private)
-    function [lowerBound,upperBound,dkFloor,dtRemain]=preEvaluate(this,t)
-      ta=this.interval.first;
-      tb=this.interval.second;
-      dt=t-ta;
-      dk=dt*this.rate;
-      dkFloor=floor(dk);
-      dtFloor=dkFloor/this.rate;
-      dtRemain=dt-dtFloor;
-      lowerBound=(t>=ta);
-      upperBound=(t<=tb);
-      dkMax=max(dk(upperBound));
-      blockIntegrate(this,ceil(dkMax)); % ceil is not floor+1 for integers
+  methods (Access = private)
+    function [lowerBound, upperBound, dkFloor, dtRemain] = preEvaluate(this, t)
+      ta = this.interval.first;
+      tb = this.interval.second;
+      dt = t-ta;
+      dk = dt*this.rate;
+      dkFloor = floor(dk);
+      dtFloor = dkFloor/this.rate;
+      dtRemain = dt-dtFloor;
+      lowerBound = (t>=ta);
+      upperBound = (t<=tb);
+      dkMax = max(dk(upperBound));
+      blockIntegrate(this, ceil(dkMax)); % ceil is not floor+1 for integers
     end
     
-    function blockIntegrate(this,K)
-      for k=this.firstNewBlock:K
-        force=block2unitforce(this.block(k));
-        this.state(:,k+1)=this.Ad*this.state(:,k)+this.Bd*force';
+    function blockIntegrate(this, K)
+      for k = this.firstNewBlock:K
+        force = block2unitforce(this.block(k));
+        this.state(:, k+1) = this.Ad*this.state(:, k)+this.Bd*force';
       end
-      this.firstNewBlock=K+1;
+      this.firstNewBlock = K+1;
     end
     
-    function substate=subIntegrate(this,kF,dt)
-      N=this.numStates;
-      sF=kF+1;
+    function substate = subIntegrate(this, kF, dt)
+      N = this.numStates;
+      sF = kF+1;
       if(dt<eps)
-        substate=this.state(:,sF);
+        substate = this.state(:, sF);
       else
-        ABsub=expmApprox(this.ABZ*dt);
-        force=block2unitforce(this.block(sF));
-        substate=ABsub*[this.state(:,sF);force']; % fast
-        substate=substate(1:N);
+        ABsub = expmApprox(this.ABZ*dt);
+        force = block2unitforce(this.block(sF));
+        substate = ABsub*[this.state(:, sF);force']; % fast
+        substate = substate(1:N);
       end
     end
   end
     
 end
 
-function force=block2unitforce(block)
-  halfIntMax=2147483647.5;
-  force=double(block.uint32)/halfIntMax-1;
+function force = block2unitforce(block)
+  halfIntMax = 2147483647.5;
+  force = double(block.uint32)/halfIntMax-1;
 end
 
-function expA=expmApprox(A)
-  expA=speye(size(A))+A+(A*A)/2;
+function expA = expmApprox(A)
+  expA = speye(size(A))+A+(A*A)/2;
 end
 
 function pose = predictPose(tP, dt)
--- trunk/components/+BoundedMarkov/BoundedMarkovConfig.m
+++ trunk/components/+BoundedMarkov/BoundedMarkovConfig.m
@@ -1,6 +1,6 @@
 classdef BoundedMarkovConfig < handle
 
-  properties (Constant=true,GetAccess=protected)   
+  properties (Constant = true, GetAccess = protected)   
     % Markov process
     % xdot = A*x + B*u
     % x = state [latLonAlt; rotation; latLonAltRate; rotationRate], double 12-by-1
@@ -9,22 +9,22 @@ classdef BoundedMarkovConfig < handle
     % B = sparse double 12-by-numInputs
     
     % state transition matrix
-    A=sparse([zeros(6),eye(6);zeros(6),zeros(6)]); 
+    A = sparse([zeros(6), eye(6); zeros(6), zeros(6)]); 
     
     % forcing matrix
-    B=sparse([zeros(6);diag([1;1;1;0.1;0.1;0.1])]); % 6-DOF
-%     B=sparse([zeros(6);diag([1,1,0,0,0,0.1])]); % 3-DOF (X,Y,Theta)
-%     B=sparse([zeros(6);diag([1,1,1,0,0,0])]); % 3-DOF (X,Y,Z)
-%     B=sparse([zeros(6);diag([1,1,0,0,0,0])]); % 2-DOF (X,Y)
+    B = sparse([zeros(6); diag([1; 1; 1; 0.1; 0.1; 0.1])]); % 6-DOF
+%     B = sparse([zeros(6); diag([1, 1, 0, 0, 0, 0.1])]); % 3-DOF (X,Y,Theta)
+%     B = sparse([zeros(6); diag([1, 1, 1, 0, 0, 0])]); % 3-DOF (X,Y,Z)
+%     B = sparse([zeros(6); diag([1, 1, 0, 0, 0, 0])]); % 2-DOF (X,Y)
 
     % Fixed initial state
-    initialPosition=[0;0;0];
-    initialRotation=[1;0;0;0];
-    initialPositionRate=[0;0;0];
-    initialOmega=[0;0;0];
+    initialPosition = [6378137.0; 0; 0]; % ([6378137.0; 0; 0]) WGS84 intersection of equator and prime meridian
+    initialRotation = [1; 0; 0; 0];
+    initialPositionRate = [0; 0; 0];
+    initialOmega = [0; 0; 0];
     
     % Model fidelity parameter (there are 32*numInputs bits per block)
-    rate=2;
+    rate = 2;
   end
   
 end
