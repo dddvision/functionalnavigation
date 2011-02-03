e11ac441aab3cfc462103d43dd943031a72ec43f
oreifej@gmail.com <oreifej@gmail.com>
2011-02-03 21:47:39 +0000

    Updating the cost computation using chi-square statistic

--- components/+FastPBM/@FastPBM/private/computeCost2.m
+++ components/+FastPBM/@FastPBM/private/computeCost2.m
@@ -2,9 +2,14 @@
 % TODO: ensure that mu=0 unless there is a strong theoretical argument against it
 % TODO: supply normpdf without depending on MATLAB toolboxes
 function cost = computeCost2(residual, mu, sigma)
-  YMax = normProb(mu, mu, sigma);
-  Y = normProb(residual, mu, sigma);
-  Y = Y/YMax;
-  cost = sum(-log(Y))/numel(Y);
+  y = sum( ( (residual - mu)/sigma ).^2 ); % Sum of normalized squared differences 
+  Pux = chi2pdf(y,length(residual)); % P(u|x)
+  infN = chi2pdf(length(residual)-2,length(residual)); % ||P(u|x)||_inf
+  cost = -log(Pux/infN);
+  
+  %YMax = normProb(mu, mu, sigma);
+  %Y = normProb(residual, mu, sigma);
+  %Y = Y/YMax;
+  %cost = sum(-log(Y))/numel(Y);
   % TODO: explain why this is divided by numel(Y)?
 end
