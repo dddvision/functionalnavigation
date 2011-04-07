51eb0d3b52399ebee660521dcd89708dd1611d96
dddvision <dddvision@gmail.com>
2011-04-07 20:37:04 +0000

    Moved some private functions into FastPBM.m. Fixed data flow in/out of FastPBM calibration method.

--- components/+FastPBM/@FastPBM/private/normProb.m
+++
@@ -1,3 +0,0 @@
-function prob = normProb(x, mu, sigma)
-  prob = exp(-0.5 * ((x - mu)./sigma).^2) ./ (sqrt(2*pi) .* sigma);
-end
--- components/+FastPBM/FastPBMConfig.m
+++ components/+FastPBM/FastPBMConfig.m
@@ -5,7 +5,7 @@ classdef FastPBMConfig < handle
     maxFeatures = 500; % (500) maximum integer number of features per frame to find
     maxSearch = 0.02; % (0.02) approximate maximum angle to search for a feature in radians
     trackerName = 'SparseTrackerKLT'; % ('SparseTrackerKLT') name of the tracker implementation to use
-    angularDeviation = 0.005; % angular deviation of the tracker in radians
+    angularDeviation = 0.004; % angular deviation of the tracker in radians
 
     % parameters that do not affect the measure
     displayFeatures = true; % (false) show tracked features
