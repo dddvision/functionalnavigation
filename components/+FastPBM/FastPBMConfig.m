fbee7644a0a3f98becf7eda14d26072a91f20b8f
dddvision <dddvision@gmail.com>
2011-04-07 23:32:40 +0000

    FastPBM.FastPBM.calibrate() is now a static function. Minor update to demo and test scripts.

--- components/+FastPBM/FastPBMConfig.m
+++ components/+FastPBM/FastPBMConfig.m
@@ -4,12 +4,11 @@ classdef FastPBMConfig < handle
     maxCost = 20; % (20) maximum cost for a single graph edge
     maxFeatures = 500; % (500) maximum integer number of features per frame to find
     maxSearch = 0.02; % (0.02) approximate maximum angle to search for a feature in radians
-    trackerName = 'SparseTrackerKLT'; % ('SparseTrackerKLT') name of the tracker implementation to use
-    angularDeviation = 0.004; % angular deviation of the tracker in radians
+    trackerName = 'SparseTrackerSURF'; % ('SparseTrackerKLT') name of the tracker implementation to use
+    angularDeviation = 0.003; % angular deviation of the tracker in radians
 
     % parameters that do not affect the measure
     displayFeatures = true; % (false) show tracked features
-    calibrate = false; % (false) compute tracker calibration statistics
     overwriteMEX = true; % (true) replace mex files if they already exist
   end
 end
