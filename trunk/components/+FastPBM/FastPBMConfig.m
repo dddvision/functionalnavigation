51e9520de755009c8de45dcb537ae2a7f37e04e4
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2011-04-12 14:05:36 +0000

    Removed the need for chi-squared Yates adjustment.

--- trunk/components/+FastPBM/FastPBMConfig.m
+++ trunk/components/+FastPBM/FastPBMConfig.m
@@ -3,12 +3,9 @@ classdef FastPBMConfig < handle
     % parameters that affect the measure
     maxFeatures = 500; % (500) maximum integer number of features per frame to find
     maxSearch = 0.02; % (0.02) approximate maximum angle to search for a feature in radians
-    trackerName = 'SparseTrackerKLT'; % ('SparseTrackerKLT') name of the tracker implementation to use
-    
-    %partitions = [0, 0.000439, 0.0028, pi];
-    partitions = [0, 5.2528e-05, 1.3324e-04, 3.1558e-04, 1.1538e-03, pi];
-    
-    %densities = [0.6648, 0.2315, 0.1037];
+    trackerName = 'SparseTrackerKLT'; % ('SparseTrackerKLT') name of the tracker implementation to use    
+    partitions = [0, 5.2528e-05, 1.3324e-04, 3.1558e-04, 1.1538e-03, pi]; % bin partitions for chi-square test
+    deviations = [1.0752e-01, 5.4398e-02, 3.8465e-02, 4.4942e-02, 7.1597e-02]; % deviation expected in each bin
     
     % parameters that do not affect the measure
     displayFeatures = false; % (false) show tracked features
