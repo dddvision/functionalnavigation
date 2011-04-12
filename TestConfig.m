8db7a2a2c264f8f8686760995e0624fd45a68872
dddvision <dddvision@gmail.com>
2011-04-12 17:38:48 +0000

    Checking in a few tweaks to the visual measure based on chi-square statistics. However, this method is problematic because it is not robust to non-normal distributions. Therefore, I am investigating what can be done with the mean statistic.

--- TestConfig.m
+++ TestConfig.m
@@ -1,6 +1,6 @@
 classdef TestConfig
   properties (Constant = true, GetAccess = public)
-    uri = 'antbed:MiddleburyTemple'; % data resource identifier
+    uri = 'antbed:MiddleburyData'; % data resource identifier
     dynamicModel = 'tom'; % dynamic model name
     measure = {'tom'}; % cell array of measure names
     initialTime = antbed.getCurrentTime(); % default initial time
