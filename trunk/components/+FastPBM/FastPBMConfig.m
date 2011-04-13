6a43d8e452770096610bb56f3c464ecca0715dce
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2011-04-13 01:25:07 +0000

    Moving to a simpler measure based on the balance between the number of features tracked above and below the epipolar plane.

--- trunk/components/+FastPBM/FastPBMConfig.m
+++ trunk/components/+FastPBM/FastPBMConfig.m
@@ -4,11 +4,10 @@ classdef FastPBMConfig < handle
     maxFeatures = 500; % (500) maximum integer number of features per frame to find
     maxSearch = 0.02; % (0.02) approximate maximum angle to search for a feature in radians
     trackerName = 'SparseTrackerKLT'; % ('SparseTrackerKLT') name of the tracker implementation to use    
-    partitions = [0, 5.2528e-05, 1.3324e-04, 3.1558e-04, 1.1538e-03, pi]; % bin partitions for chi-square test
-    deviations = [2.0171e-01, 1.0197e-01, 7.2066e-02, 8.4272e-02, 1.3446e-01]; % expected deviation in each bin
-      
+    deviation = 0.17; % unitless tracker error parameter
+    
     % parameters that do not affect the measure
-    displayFeatures = false; % (false) show tracked features
+    displayFeatures = true; % (false) show tracked features
     overwriteMEX = true; % (true) replace mex files if they already exist
   end
 end
