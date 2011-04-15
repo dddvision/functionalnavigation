fc2965c95a5b62f5211248a9dbe17ca3a4a9adc0
dddvision <dddvision@gmail.com>
2011-04-15 03:51:15 +0000

    Adding preliminary XBOX Kinect data container.

---
+++ components/+XBOXKinect/XBOXKinect.m
@@ -0,0 +1,69 @@
+classdef XBOXKinect < XBOXKinect.XBOXKinectConfig & antbed.DataContainer
+  properties (Constant=true,GetAccess=private)
+    hasRef=false;
+    bodyRef=[];
+    noRefText='MacBook cannot supply a reference trajectory';
+    notMacText='This data source depends on MacBook or MacBook Pro laptop hardware';
+    camDescription=['MacBook builtin iSight camera in low resolution mode. ',...
+        'Depends on VLC for access. Clear the sensor instance to stop recording.'];
+    kinectDescription='XBOX Kinect';
+  end
+  
+  properties (Access=private)
+    sensors
+    sensorDescription
+  end
+  
+  methods (Static=true,Access=public)
+    function initialize(name)
+      function text=componentDescription
+        text=['Provides data from the built-in camera in most MacBook and MacBook Pro laptops ',...
+          'and an XBOX Kinect sensor.'];
+      end
+      antbed.DataContainer.connect(name,@componentDescription,@XBOXKinect.XBOXKinect);
+    end
+  end
+  
+  methods (Access=public)
+    function this=XBOXKinect(initialTime)
+      this=this@antbed.DataContainer(initialTime);
+
+      if(~ismac)
+        error(this.notMacText);
+      end
+
+      this.sensorDescription{1}=this.camDescription;
+      this.sensorDescription{2}=this.kinectDescription;
+      this.sensors{1}=XBOXKinect.MacCam(initialTime);
+      this.sensors{2}=XBOXKinect.Kinect(initialTime);
+    end
+    
+    function list=listSensors(this,type)
+      K=numel(this.sensors);
+      flag=false(K,1);
+      for k=1:K
+        if(isa(this.sensors{k},type))
+          flag(k)=true;
+        end
+      end
+      list=antbed.SensorIndex(find(flag)-1);
+    end
+    
+    function text=getSensorDescription(this,id)
+      text=this.sensorDescription{id+1};
+    end
+    
+    function obj=getSensor(this,id)
+      obj=this.sensors{id+1};
+    end
+    
+    function flag=hasReferenceTrajectory(this)
+      flag=this.hasRef;
+    end
+    
+    function x=getReferenceTrajectory(this)
+      x=this.bodyRef;
+      error(this.noRefText);
+    end
+  end
+end
---
+++ components/+XBOXKinect/kinectapp.cpp
@@ -0,0 +1,210 @@
+#include "libfreenect.hpp"
+#include <pthread.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/time.h>
+#include <signal.h>
+
+static bool die = false;
+void interrupt(int signum)
+{ 
+  if( (signum==SIGINT)|(signum==SIGTERM) )
+  {
+    die = true;
+  }
+  return;
+}
+
+namespace tom
+{
+  /**
+   * This class represents a world time system
+   *
+   * NOTES
+   * The default reference is GPS time at the prime meridian in seconds since 1980 JAN 06 T00:00:00
+   * Choosing another time system may adversely affect interoperability between framework classes
+   */
+  typedef double WorldTime;
+}
+
+namespace antbed
+{
+  /**
+   * Get the current time of day from the operating system
+   *
+   * @return current system time in tom.WorldTime format
+   *
+   * NOTES
+   * @see tom::WorldTime
+   */
+  tom::WorldTime getCurrentTime(void)
+  {
+    timeval tv;
+    long int offset = 315964800; // difference between Jan 6 1980 and Jan 1 1979
+    double time;
+    gettimeofday(&tv, NULL);
+    time = static_cast<double> (tv.tv_sec-offset)+static_cast<double> (tv.tv_usec)/1000000.0;
+    return (time);
+  }
+}
+
+class Mutex
+{
+public:
+	Mutex()
+  {
+		pthread_mutex_init( &m_mutex, NULL );
+	}
+	void lock()
+  {
+		pthread_mutex_lock( &m_mutex );
+	}
+	void unlock()
+  {
+		pthread_mutex_unlock( &m_mutex );
+	}
+private:
+	pthread_mutex_t m_mutex;
+};
+
+class MyFreenectDevice : public Freenect::FreenectDevice
+{
+public:
+	MyFreenectDevice(freenect_context *_ctx, int _index) : Freenect::FreenectDevice(_ctx, _index), 
+    m_buffer_depth(FREENECT_DEPTH_11BIT_SIZE), 
+    m_buffer_video(FREENECT_VIDEO_RGB_SIZE), 
+    depth(FREENECT_DEPTH_11BIT_SIZE), 
+    video(FREENECT_VIDEO_RGB_SIZE), 
+    m_new_rgb_frame(false), 
+    m_new_depth_frame(false)
+	{}
+  
+  // Do not call directly even in child
+	void DepthCallback(void* _depth, uint32_t timestamp)
+  {
+		m_depth_mutex.lock();
+		uint8_t* depth = static_cast<uint8_t*>(_depth);
+    std::copy(depth, depth+FREENECT_DEPTH_11BIT_SIZE, m_buffer_depth.begin());
+		m_new_depth_frame = true;
+		m_depth_mutex.unlock();
+	}
+  
+	// Do not call directly even in child
+	void VideoCallback(void* _rgb, uint32_t timestamp)
+  {
+		m_rgb_mutex.lock();
+		uint8_t* rgb = static_cast<uint8_t*>(_rgb);
+		std::copy(rgb, rgb+FREENECT_VIDEO_RGB_SIZE, m_buffer_video.begin());
+		m_new_rgb_frame = true;
+		m_rgb_mutex.unlock();
+	};
+  
+  bool getDepth(std::vector<uint8_t> &buffer)
+  {
+		m_depth_mutex.lock();
+		if(m_new_depth_frame)
+    {
+			buffer.swap(m_buffer_depth);
+			m_new_depth_frame = false;
+			m_depth_mutex.unlock();
+			return(true);
+		}
+    else
+    {
+			m_depth_mutex.unlock();
+			return(false);
+		}
+	}
+  
+	bool getRGB(std::vector<uint8_t> &buffer)
+  {
+		m_rgb_mutex.lock();
+		if(m_new_rgb_frame)
+    {
+			buffer.swap(m_buffer_video);
+			m_new_rgb_frame = false;
+			m_rgb_mutex.unlock();
+			return(true);
+		}
+    else
+    {
+			m_rgb_mutex.unlock();
+			return(false);
+		}
+	}
+  
+  void recordRawData(void)
+  {
+    static unsigned int count = 0;
+    static char str[256];
+
+    updateState();
+
+    if(getDepth(depth))
+    {
+      getRGB(video);
+
+      sprintf(str, "time%06u.dat", count);
+      timeFile.open(str, std::ios::out | std::ios::binary);
+      sprintf(str, "%016.6lf", antbed::getCurrentTime());
+      timeFile << str << std::endl;
+      timeFile.close();
+
+      sprintf(str, "depth%06u.dat", count);
+      depthFile.open(str, std::ios::out | std::ios::binary);
+      depthFile.write(reinterpret_cast<char*>(&depth[0]), FREENECT_DEPTH_11BIT_SIZE);
+      depthFile.close();
+
+      sprintf(str, "video%06u.dat", count);
+      videoFile.open(str, std::ios::out | std::ios::binary);    
+      videoFile.write(reinterpret_cast<char*>(&video[0]), FREENECT_VIDEO_RGB_SIZE);
+      videoFile.close();
+
+      ++count;
+    }
+
+    return;
+  }
+
+private:
+ 	std::vector<uint8_t> m_buffer_depth;
+	std::vector<uint8_t> m_buffer_video;
+  std::vector<uint8_t> depth;
+  std::vector<uint8_t> video;
+  std::ofstream depthFile;
+  std::ofstream videoFile;
+  std::ofstream timeFile;
+	Mutex m_rgb_mutex;
+	Mutex m_depth_mutex;
+	bool m_new_rgb_frame;
+	bool m_new_depth_frame;
+};
+
+int main(int argc, char **argv)
+{
+  Freenect::Freenect freenect;
+	MyFreenectDevice* device = &freenect.createDevice<MyFreenectDevice>(0);
+
+  signal(SIGINT,&interrupt);
+  
+  device->startVideo();
+	device->startDepth();
+  
+  while(!die)
+  {
+    device->recordRawData();
+    usleep(5);
+  }
+  
+  device->stopDepth();
+	device->stopVideo();
+  
+  freenect.deleteDevice(0);
+  
+	return(0);
+}
