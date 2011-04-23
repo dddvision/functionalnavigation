a8b94ea3fabdaa335d649c15e6ce3e41a1f204a6
dddvision <dddvision@gmail.com>
2011-04-23 01:03:11 +0000

    Miscelaneous bug fixes, speedups, and configuration changes. See difference log.

--- DemoDisplay.m
+++ DemoDisplay.m
@@ -1,5 +1,14 @@
 classdef DemoDisplay < DemoConfig & handle
   
+  properties (Constant = true, GetAccess = private)
+    gamma = 2; % (2) nonlinearity of trajectory transparency
+    bigSteps = 10; % (10) number of body frames per trajectory
+    subSteps = 10; % (10) number of line segments between body frames
+    colorBackground = [1, 1, 1]; % ([1, 1, 1]) color of figure background
+    colorHighlight = [1, 0, 0]; % ([1, 0, 0]) color of objects to emphasize
+    colorReference = [0, 1, 0]; % ([0, 1, 0]) color of reference objects 
+  end
+  
   properties (SetAccess = private, GetAccess = private)
     hfigure
     haxes
--- components/+FastPBM/FastPBMConfig.m
+++ components/+FastPBM/FastPBMConfig.m
@@ -4,7 +4,7 @@ classdef FastPBMConfig < handle
     trackerName = 'KLTOpenCV'; % ('KLT'), 'KLTOpenCV', 'SURF' tracker implementation to use
     maxFeatures = 500; % (500) maximum integer number of features per frame to find
     maxSearch = 0.02; % (0.02) approximate maximum angle to search for a feature in radians
-    deviation = 0.16; % unitless tracker error parameter
+    deviation = 0.03; % unitless tracker error parameter
     
     % parameters that do not affect the measure
     verbose = true; % (true) display warnings and other diagnostic information
