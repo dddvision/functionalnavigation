2fc9820a0831d9ddd8a432f5eba5f752494d3d92
Fenelon.Patrick <Fenelon.Patrick@fc5c3f66-62be-11de-b126-f3c85089a223>
2011-05-19 06:21:05 +0000

    Fixed bug in TrajectoryPerturbation involving interpretation of time. Finished calculation of monotonicity and granularity. Added testing for perturbations of all dimensions and each dimension separately. Added data summary at end of test.

--- trunk/+antbed/MeasureTest.m
+++ trunk/+antbed/MeasureTest.m
@@ -98,89 +98,144 @@ classdef MeasureTest < handle
         end
       end
         
-%       if(~measure.hasData())
-%         fprintf('\nwarning: Skipping measure characterization. Measure has no data.');
-%       elseif(~strncmp(uri, 'antbed:', 7))
-%         fprintf('\nwarning: Skipping measure characterization. URI scheme not recognized');
-%       else
-%         resource = uri(8:end);
-%         container = antbed.DataContainer.create(resource, initialTime);
-%         if(~container.hasReferenceTrajectory())
-%           fprintf('\nwarning: Skipping measure characterization. No reference trajectory is available.');
-%         else
-%           edgeList = measure.findEdges(measure.first(), measure.last(), measure.first(), measure.last());
-%           baseTrajectory = antbed.TrajectoryPerturbation(trajectory.evaluate(interval.first), interval);
-%           
-%           edgeList
-%           
-%           if numel(edgeList) ~= 0
-%             zeroPose = tom.Pose;
-%             zeroPose.p = [0; 0; 0];
-%             zeroPose.q = [1; 0; 0; 0];
-%             baseTrajectory.setPerturbation(zeroPose);
-%             basePose = baseTrajectory.evaluate(0);
-%             display(basePose);
-%             edgeCosts = 0:size(edgeList);
-%             
-%             %Compute cost at ground truth, save for bias and granularity
-%             %computations
-%             for k = 1:numel(edgeList)
-%               edgeCosts(k) = measure.computeEdgeCost(baseTrajectory, edgeList(k));
-%             end
-%             
-%             %Set Trajectory Perturbation to matlab machine eps
-%             epsPose = tom.Pose;
-%             epsPose.p = [eps('double'); eps('double'); eps('double')];
-%             axisAng = [eps('double'); eps('double'); eps('double')];
-%             epsPose.q = AxisAngle2Quat(axisAng);
-%             baseTrajectory.setPerturbation(epsPose);
-%             %double eps utill a different cost is returned
-%             while 1
-%               for k = 1:numel(edgeList)
-%                 tmp = measure.computeEdgeCost(baseTrajectory, edgeList(k));
-%                 if tmp ~= edgeCosts(k)
-%                   break;
-%                 end
-%               end
-%               epsPose.p = 2 * epsPose.p;
-%               axisAng = 2 * axisAng;
-%               epsPose.q = AxisAngle2Quat(axisAng);
-%               baseTrajectory.setPerturbation(zeroPose);
-%               fprintf('Testing with value %f, cost: %f\n', epsPose.p(1), edgeCosts(1));
-%             end
-%             
-%             %This becomes the granularity
-%             Granularity = epsPose.p(1);
-%             Bias =  mean(edgeCosts);
-%             
-%             
-%             fprintf('Granularity: %f', Granularity);
-%             fprintf('Bias: %f', Bias);
-%             
-%             %Compute Monotonicity
-%             %construct sample space based off granularity
-%             MonotinicityAry = zeros(10, size(edgeList));
-%             for i = 1:10
-%               for k = 1:numel(edgeList)
-%                 tmp = measure.computeEdgeCost(baseTrajectory, edgeList(k));
-%                 MonotinicityAry(i,k) = 2* (tmp - edgeCosts(k)) / epsPose.p(1);
-%                 edgeCosts(k) = tmp;
-%               end
-%               epsPose.p = 2 * epsPose.p;
-%               axisAng = 2 * axisAng;
-%               epsPose.q = AxisAngle2Quat(axisAng);
-%               baseTrajectory.setPerturbation(zeroPose);
-%             end
-%             
-%             Monotinicity = mean(mean(MonotinicityAry));
-%             fprintf('Monotinicity: %f', Monotinicity);
-%             
-%           else
-%             fprintf('\nwarning: Skipping measure characterization. Measure has no edges.');
-%           end
-%           
-%         end
-%       end
+      if(~measure.hasData())
+        fprintf('\nwarning: Skipping measure characterization. Measure has no data.');
+      elseif(~strncmp(uri, 'antbed:', 7))
+        fprintf('\nwarning: Skipping measure characterization. URI scheme not recognized');
+      else
+        resource = uri(8:end);
+        container = antbed.DataContainer.create(resource, initialTime);
+        if(~container.hasReferenceTrajectory())
+          fprintf('\nwarning: Skipping measure characterization. No reference trajectory is available.');
+        else
+          edgeList = measure.findEdges(measure.first(), measure.last(), measure.first(), measure.last());
+          baseTrajectory = antbed.TrajectoryPerturbation(trajectory);
+          
+          if numel(edgeList) ~= 0
+            Granularity = zeros(1,7);
+            Bias = zeros(1,7);
+            Monotinicity = zeros(1,7);
+            % (1) preturb in all directions
+            % (2) preturb in 1st translation dimension
+            % (3) preturb in 2nd translation dimension
+            % (4) preturb in 3rd translation dimension
+            % (5) preturb in 1st rotation dimension
+            % (6) preturb in 2nd rotation dimension
+            % (7) preturb in 3rd rotation dimension
+            
+            % axis angle matrix
+            axisAng = [eps('double'),0,0,0,eps('double'),0,0; eps('double'),0,0,0,0,eps('double'),0; eps('double'),0,0,0,0,0,eps('double')];
+            
+            epsPose = repmat(tom.Pose, [1,7]);
+            
+            epsPose(1).p = [eps('double'); eps('double'); eps('double')];
+            epsPose(1).q = AxisAngle2Quat(axisAng(:, 1));
+            epsPose(2).p = [eps('double'); 0; 0];
+            epsPose(2).q = AxisAngle2Quat(axisAng(:, 2));
+            epsPose(3).p = [0; eps('double'); 0];
+            epsPose(3).q = AxisAngle2Quat(axisAng(:, 3));
+            epsPose(4).p = [0; 0; eps('double')];
+            epsPose(4).q = AxisAngle2Quat(axisAng(:, 4));
+            epsPose(5).p = [0; 0; 0];
+            epsPose(5).q = AxisAngle2Quat(axisAng(:, 5));
+            epsPose(6).p = [0; 0; 0];
+            epsPose(6).q = AxisAngle2Quat(axisAng(:, 6));
+            epsPose(7).p = [0; 0; 0];
+            epsPose(7).q = AxisAngle2Quat(axisAng(:, 7));
+            
+            %iterate over the 7 types of tested perturbation
+            msg = {'All translation and rotation dimensions' ; ...
+                   'Translation dimension 1' ; ...
+                   'Translation dimension 2' ; ...
+                   'Translation dimension 3' ; ...
+                   'Rotation dimension 1' ; ...
+                   'Rotation dimension 2' ; ...
+                   'Rotation dimension 3'};
+            for e = 1:7  
+              fprintf('* Current Dim: %s *\n',msg{e});
+              zeroPose = tom.Pose;
+              zeroPose.p = [0; 0; 0];
+              zeroPose.q = [1; 0; 0; 0];
+              currEps = eps('double');
+              baseTrajectory.setPerturbation(zeroPose);
+              edgeCosts = 0:size(edgeList);
+
+              %Compute cost at ground truth, save for bias and granularity
+              %computations
+              for k = 1:numel(edgeList)
+                edgeCosts(k) = measure.computeEdgeCost(baseTrajectory, edgeList(k));
+              end
+              fprintf('Ground Truth cost: %f\n', edgeCosts(1));
+
+              %Set Trajectory Perturbation to matlab machine eps
+              baseTrajectory.setPerturbation(epsPose(e));
+
+              %double eps utill a different cost is returned
+              step = 0;
+              %step through doubleing distance everytime
+              for i = 1:64
+                stop = 0;
+                step = i;
+                result = 1:k;
+                %step through each edge
+                for k = 1:numel(edgeList)
+                  result(k) = measure.computeEdgeCost(baseTrajectory, edgeList(k));
+                  if result(k) ~= edgeCosts(k)
+                    stop = 1;
+                    break;
+                  end
+                end
+                currEps = 2 * currEps;
+                epsPose(e).p = 2 .* epsPose(e).p;
+                axisAng(:,e) = 2 .* axisAng(:,e);
+                epsPose(e).q = AxisAngle2Quat(axisAng(:,e));
+                baseTrajectory.setPerturbation(epsPose(e));
+                fprintf('Testing with value %f, cost: %f\n', currEps, result(1));
+                if(stop==1)
+                  break;
+                end
+              end
+
+              %This becomes the granularity
+              Granularity(e) = currEps;
+              if(step == 64)
+                Granularity(e) = inf;
+              end
+              Bias(e) =  mean(edgeCosts);
+
+
+
+              %Compute Monotonicity
+              %construct sample space based off granularity
+              MonotinicityAry = zeros(10, numel(edgeList));
+              for i = 1:10
+                for k = 1:numel(edgeList)
+                  tmp = measure.computeEdgeCost(baseTrajectory, edgeList(k));
+                  MonotinicityAry(i,k) = 2* (tmp - edgeCosts(k)) / currEps;
+                  edgeCosts(k) = tmp;
+                end
+                currEps = 2 * currEps;
+                epsPose(e).p = 2 * epsPose(e).p;
+                axisAng(:,e) = 2 .* axisAng(:,e);
+                epsPose(e).q = AxisAngle2Quat(axisAng);
+                baseTrajectory.setPerturbation(epsPose(e));
+              end
+
+              Monotinicity(e) = mean(mean(MonotinicityAry));
+
+            end
+            for e = 1:7
+              fprintf('%s',msg{e});
+              fprintf('\nGranularity: %f\n', Granularity(e));
+              fprintf('Bias: %f\n', Bias(e));
+              fprintf('Monotinicity: %f\n\n', Monotinicity(e));
+            end
+          else
+            fprintf('\nwarning: Skipping measure characterization. Measure has no edges.');
+          end
+          
+        end
+      end
       
       fprintf('\n\n*** End Measure Test ***');
     end
@@ -189,26 +244,26 @@ classdef MeasureTest < handle
   
 end
 
-% function q = AxisAngle2Quat(v)
-%   v1 = v(1, :);
-%   v2 = v(2, :);
-%   v3 = v(3, :);
-%   n = sqrt(v1.*v1+v2.*v2+v3.*v3);
-%   good = n>eps;
-%   ngood = n(good);
-%   N = numel(n);
-%   a = zeros(1, N);
-%   b = zeros(1, N);
-%   c = zeros(1, N);
-%   th2 = zeros(1, N);
-%   a(good) = v1(good)./ngood;
-%   b(good) = v2(good)./ngood;
-%   c(good) = v3(good)./ngood;
-%   th2(good) = ngood/2;
-%   s = sin(th2);
-%   q1 = cos(th2);
-%   q2 = s.*a;
-%   q3 = s.*b;
-%   q4 = s.*c;
-%   q = [q1; q2; q3; q4];
-% end
+function q = AxisAngle2Quat(v)
+  v1 = v(1, :);
+  v2 = v(2, :);
+  v3 = v(3, :);
+  n = sqrt(v1.*v1+v2.*v2+v3.*v3);
+  good = n>eps;
+  ngood = n(good);
+  N = numel(n);
+  a = zeros(1, N);
+  b = zeros(1, N);
+  c = zeros(1, N);
+  th2 = zeros(1, N);
+  a(good) = v1(good)./ngood;
+  b(good) = v2(good)./ngood;
+  c(good) = v3(good)./ngood;
+  th2(good) = ngood/2;
+  s = sin(th2);
+  q1 = cos(th2);
+  q2 = s.*a;
+  q3 = s.*b;
+  q4 = s.*c;
+  q = [q1; q2; q3; q4];
+end
--- trunk/+antbed/TrajectoryPerturbation.m
+++ trunk/+antbed/TrajectoryPerturbation.m
@@ -1,37 +1,42 @@
 classdef TrajectoryPerturbation < tom.Trajectory
 
   properties (GetAccess = private, SetAccess = private)
-      basePose
-      deltaPose
-      domainInterval
+      deltaPoseUnit
+      baseTrajectory
   end
     
   methods (Access = public, Static = true)
     % base trajectory and rotation (ie ground truth) passed as a tom.pose
-    function this = TrajectoryPerturbation(pose,interval)
-        this.basePose = pose;
-        this.domainInterval = interval;
+    function this = TrajectoryPerturbation(refTrajectory)
+      this.baseTrajectory = refTrajectory;
     end
   end
     
   methods (Access = public, Static = false)
     function interval = domain(this)
-      interval = this.domainInterval;
+      interval = domain(this.baseTrajectory);
     end
     function setPerturbation(this, offsetPose)
-      this.deltaPose = offsetPose;
+      this.deltaPoseUnit = offsetPose;
     end
     
     function pose = evaluate(this, t)
+      interval = this.baseTrajectory.domain();
+      initialTime = interval.first;
+            
       N = numel(t);
       if(N==0)
         pose = repmat(tom.Pose, [1, 0]);
       else
+        deltaTime = t-initialTime;                       
         pose(1, N) = tom.Pose;
         %TODO: Add interpolation for non-diecrete time values
-        for k = 1:N
-          pose(k).p = this.basePose.p + this.deltaPose.p;
-          pose(k).q = Quat2Homo(this.deltaPose.q) * this.basePose.q;
+        for k = 1:size(t)
+          translation = deltaTime(k) * this.deltaPoseUnit.p;
+          axisAngle = Quat2AxisAngle(this.deltaPoseUnit.q) * deltaTime(k);
+          basePose = this.baseTrajectory.evaluate(t(k));
+          pose(k).p = basePose.p + translation;
+          pose(k).q = Quat2Homo(AxisAngle2Quat(axisAngle)) * basePose.q;
         end
       end   
     end
@@ -64,4 +69,52 @@ function h = Quat2Homo(q)
        [q2,  q1, -q4,  q3]
        [q3,  q4,  q1, -q2]
        [q4, -q3,  q2,  q1]];
+end
+
+function q = AxisAngle2Quat(v)
+  v1 = v(1, :);
+  v2 = v(2, :);
+  v3 = v(3, :);
+  n = sqrt(v1.*v1+v2.*v2+v3.*v3);
+  good = n>eps;
+  ngood = n(good);
+  N = numel(n);
+  a = zeros(1, N);
+  b = zeros(1, N);
+  c = zeros(1, N);
+  th2 = zeros(1, N);
+  a(good) = v1(good)./ngood;
+  b(good) = v2(good)./ngood;
+  c(good) = v3(good)./ngood;
+  th2(good) = ngood/2;
+  s = sin(th2);
+  q1 = cos(th2);
+  q2 = s.*a;
+  q3 = s.*b;
+  q4 = s.*c;
+  q = [q1; q2; q3; q4];
+end
+function v = Quat2AxisAngle(q)   
+  q1 = q(1, :);
+  q2 = q(2, :);
+  q3 = q(3, :);
+  q4 = q(4, :);
+
+  scale = 2 * acos(q1);   
+  q1Sq = q1 .* q1;
+  demon = sqrt(1-q1Sq);
+  v=zeros(3,size(q,2));
+  for i=1:size(q,2)
+    if(q1(i)==1)
+      v(:,i) = [0;0;0];
+    else    
+      a(1) = q2(i) / demon(i);
+      a(2) = q3(i) / demon(i);
+      a(3) = q4(i) / demon(i);   
+      aNorm = a / norm(a);
+
+      vT = aNorm * scale(i);
+      v(:,i) = vT';
+    end   
+  end
 end
