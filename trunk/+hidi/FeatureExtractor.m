6d134d5b02f1ec8a05c022b06430c51384eeda05
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2012-04-17 14:46:45 +0000

    Added feature concatenation method to FeatureExtractor

--- trunk/+hidi/FeatureExtractor.m
+++ trunk/+hidi/FeatureExtractor.m
@@ -1,4 +1,27 @@
 classdef FeatureExtractor < handle
+
+  methods (Static = true, Access = public)
+    % Extract and concatenate all features.
+    %
+    % @param[in] package       sensor package
+    % @param[in] partition     vector of data partitions
+    % @param[in] extractorList list of instantiated feature extractor objects
+    function features = cat(package, partition, extractorList)
+      numExtractors = numel(extractorList);
+      numFeatures = zeros(numExtractors, 1);
+      for index = 1:numExtractors
+        numFeatures(index) = extractorList{index}.numElements();
+      end
+      features = zeros(sum(numFeatures), 1);
+      offset = 0;
+      for index = 1:numExtractors
+        N = numFeatures(index);
+        features(offset+(1:N)) = extractorList{index}.extract(package, partition);
+        offset = offset+N;
+      end
+    end
+  end
+  
   methods (Abstract = true, Access = public)
     % Get number of elements that will be returned by the extract function.
     %
@@ -11,7 +34,7 @@ classdef FeatureExtractor < handle
     % return element name
     name = getName(this, index);
     
-    % Extract features from sensor data
+    % Extract features from sensor data.
     %
     % @param[in] sensor    multi-sensor object
     % @param[in] partition list of heel strike indices in ascending order
