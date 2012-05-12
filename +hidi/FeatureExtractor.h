95c7663e4f7eb5169941e51174cf1eb092b15235
dddvision <dddvision@gmail.com>
2012-05-12 04:16:32 +0000

    Moved FeatureExtractor into namespace hidi

--- +hidi/FeatureExtractor.h
+++ +hidi/FeatureExtractor.h
@@ -3,31 +3,34 @@
 
 #include <string>
 
-class FeatureExtractor
+namespace hidi
 {
-public:  
-  /**
-   * Get the number of features available.
-   *
-   * @return number of features
-   */ 
-  virtual size_t size(void) = 0; 
+  class FeatureExtractor
+  {
+  public:  
+    /**
+     * Get the number of features available.
+     *
+     * @return number of features
+     */ 
+    virtual size_t size(void) = 0; 
 
-  /**
-   * Get the name of a feature.
-   *
-   * @param[in] index feature index
-   * return           feature name
-   */
-  virtual std::string getName(size_t index) = 0;
-  
-  /**
-   * Get the value of a feature.
-   *
-   * @param[in] index feature index
-   * @return          feature value
-   */
-  virtual double getValue(size_t index) = 0;
-};
+    /**
+     * Get the name of a feature.
+     *
+     * @param[in] index feature index
+     * return           feature name
+     */
+    virtual std::string getName(size_t index) = 0;
+
+    /**
+     * Get the value of a feature.
+     *
+     * @param[in] index feature index
+     * @return          feature value
+     */
+    virtual double getValue(size_t index) = 0;
+  };
+}
 
 #endif
