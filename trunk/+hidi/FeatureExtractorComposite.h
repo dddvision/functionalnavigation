c8e97e463bb6eea86667ded96cb2f1ef5037b1cc
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2012-05-12 04:16:32 +0000

    Moved FeatureExtractor into namespace hidi

--- trunk/+hidi/FeatureExtractorComposite.h
+++ trunk/+hidi/FeatureExtractorComposite.h
@@ -5,67 +5,70 @@
 #include <vector>
 #include "FeatureExtractor.h"
 
-class FeatureExtractorComposite : FeatureExtractor
+namespace hidi
 {
-public:  
-  size_t size(void)
+  class FeatureExtractorComposite : FeatureExtractor
   {
-    return (pLookup()->size());
-  }
-  
-  std::string getName(size_t index)
-  {
-    size_t extractorIndex;
-    size_t featureIndex;
-    if(index>=size())
+  public:  
+    size_t size(void)
     {
-      throw("Feature index is out of range.");
+      return (pLookup()->size());
     }
-    extractorIndex = (*pLookup())[index].first;
-    featureIndex = (*pLookup())[index].second;
-    return ((*pExtractors())[extractorIndex]->getName(featureIndex));
-  }
-  
-  double getValue(size_t index)
-  {
-    size_t extractorIndex;
-    size_t featureIndex;
-    if(index>=size())
-    {
-      throw("Feature index is out of range.");
-    }
-    extractorIndex = (*pLookup())[index].first;
-    featureIndex = (*pLookup())[index].second;
-    return ((*pExtractors())[extractorIndex]->getValue(featureIndex));
-  }
-  
-  static void connect(FeatureExtractor* featureExtractor)
-  {
-    std::pair<size_t, size_t> item(pExtractors()->size(), 0);
-    size_t N = featureExtractor->size();
-    size_t n;
-    for(n = 0; n<N; ++n)
-    {
-      item.second = n;
-      pLookup()->push_back(item);
-    }
-    pExtractors()->push_back(featureExtractor);
-    return;
-  }
 
-protected:
-  typedef std::pair<size_t, size_t> ExtractorFeaturePair;
-  static std::vector<ExtractorFeaturePair>* pLookup(void)
-  {
-    static std::vector<ExtractorFeaturePair> lookup;
-    return (&lookup);
-  }
-  
-  static std::vector<FeatureExtractor*>* pExtractors(void)
-  {
-    static std::vector<FeatureExtractor*> extractors;
-    return (&extractors);
-  }
-};
+    std::string getName(size_t index)
+    {
+      size_t extractorIndex;
+      size_t featureIndex;
+      if(index>=size())
+      {
+        throw("Feature index is out of range.");
+      }
+      extractorIndex = (*pLookup())[index].first;
+      featureIndex = (*pLookup())[index].second;
+      return ((*pExtractors())[extractorIndex]->getName(featureIndex));
+    }
+
+    double getValue(size_t index)
+    {
+      size_t extractorIndex;
+      size_t featureIndex;
+      if(index>=size())
+      {
+        throw("Feature index is out of range.");
+      }
+      extractorIndex = (*pLookup())[index].first;
+      featureIndex = (*pLookup())[index].second;
+      return ((*pExtractors())[extractorIndex]->getValue(featureIndex));
+    }
+
+    static void connect(FeatureExtractor* featureExtractor)
+    {
+      std::pair<size_t, size_t> item(pExtractors()->size(), 0);
+      size_t N = featureExtractor->size();
+      size_t n;
+      for(n = 0; n<N; ++n)
+      {
+        item.second = n;
+        pLookup()->push_back(item);
+      }
+      pExtractors()->push_back(featureExtractor);
+      return;
+    }
+
+  protected:
+    typedef std::pair<size_t, size_t> ExtractorFeaturePair;
+    static std::vector<ExtractorFeaturePair>* pLookup(void)
+    {
+      static std::vector<ExtractorFeaturePair> lookup;
+      return (&lookup);
+    }
+
+    static std::vector<FeatureExtractor*>* pExtractors(void)
+    {
+      static std::vector<FeatureExtractor*> extractors;
+      return (&extractors);
+    }
+  };
+}
 
 #endif
