216f496e334a9fd6ff6e0cb5af3671aeff9285de
dddvision <dddvision@gmail.com>
2012-07-30 17:26:18 +0000

    MATLAB version of Pedometer now matches C++ version

--- +hidi/Pedometer.m
+++ +hidi/Pedometer.m
@@ -1,5 +1,10 @@
 classdef Pedometer < hidi.Sensor
   methods (Static = true, Access = protected)
+    function this = Pedometer()
+    end
+  end
+  
+  methods (Abstract = true, Access = protected)
     flag = isComplete(this, node);
     magnitude = getMagnitude(this, node);
     sigma = getDeviation(this, node);
