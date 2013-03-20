774600fb5f7f344ff292edcc1638a33eada563d5
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2013-03-20 18:21:14 +0000

    Updating the Pedometer class

--- trunk/+hidi/SensorType.h
+++ trunk/+hidi/SensorType.h
@@ -10,11 +10,12 @@ namespace hidi
    */
   typedef enum
   {
-    ACCELEROMETER_ARRAY,
-    GYROSCOPE_ARRAY,
-    MAGNETOMETER_ARRAY,
-    ALTIMETER,
-    GPS_RECEIVER
+    ACCELEROMETER_ARRAY = static_cast<uint32_t>(0),
+    GYROSCOPE_ARRAY = static_cast<uint32_t>(1),
+    MAGNETOMETER_ARRAY = static_cast<uint32_t>(2),
+    ALTIMETER = static_cast<uint32_t>(3),
+    GPS_RECEIVER = static_cast<uint32_t>(4),
+    PEDOMETER = static_cast<uint32_t>(5)
   } SensorType;
 }
   
