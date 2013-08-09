992837a38c4d7cfe6cd9c8db9475f4722ff9324e
dddvision <dddvision@fc5c3f66-62be-11de-b126-f3c85089a223>
2013-08-09 16:45:03 +0000

    Superficial update to DepthCamera

--- trunk/+hidi/DepthCamera.h
+++ trunk/+hidi/DepthCamera.h
@@ -29,47 +29,47 @@ namespace hidi
     {}
 
   public:
-   /**
-    * Get number of pixels in the non-contiguous dimension of each depth image.
-    *
-    * @return number of strides
-    */
-   virtual uint32_t numStrides(void) = 0;
-    
-   /**
-    * Get number of pixels in the contiguous dimension of each depth image.
-    *
-    * @return number of steps
-    */
-   virtual uint32_t numSteps(void) = 0;
-    
-   /**
-    * Project unit magnitude ray vectors in the camera frame to points in the depth image.
-    *
-    * @param[in]  f      forward component of the ray
-    * @param[in]  r      right component of the ray
-    * @param[in]  d      down component of the ray
-    * @param[out] stride coordinate in the non-contiguous image dimension associated with the ray
-    * @param[out] step   coordinate in the contiguous image dimension associated with the ray
-    *
-    * @note
-    * Invalid regions are identified by NAN outputs.
-    */
-   virtual void projection(const double& f, const double& r, const double& d, double& stride, double& step) = 0;
-    
-   /**
-    * Project points in the depth image to unit magnitude ray vectors in the camera frame.
-    *
-    * @param[in]  stride coordinate in the non-contiguous image dimension
-    * @param[in]  step   coordinate in the contiguous image dimension
-    * @param[out] f      forward component of the ray associated with the image coordinate
-    * @param[out] r      right component of the ray associated with the image coordinate
-    * @param[out] d      down component of the ray associated with the image coordinate
-    *
-    * @note
-    * Invalid regions are identified by NAN outputs.
-    */
-   virtual void inverseProjection(const double& stride, const double& step, double& f, double& r, double& d) = 0;
+    /**
+     * Get number of pixels in the non-contiguous dimension of each depth image.
+     *
+     * @return number of strides
+     */
+    virtual uint32_t numStrides(void) = 0;
+
+    /**
+     * Get number of pixels in the contiguous dimension of each depth image.
+     *
+     * @return number of steps
+     */
+    virtual uint32_t numSteps(void) = 0;
+
+    /**
+     * Project unit magnitude ray vectors in the camera frame to points in the depth image.
+     *
+     * @param[in]  f      forward component of the ray
+     * @param[in]  r      right component of the ray
+     * @param[in]  d      down component of the ray
+     * @param[out] stride coordinate in the non-contiguous image dimension associated with the ray
+     * @param[out] step   coordinate in the contiguous image dimension associated with the ray
+     *
+     * @note
+     * Invalid regions are identified by NAN outputs.
+     */
+    virtual void projection(const double& f, const double& r, const double& d, double& stride, double& step) = 0;
+
+    /**
+     * Project points in the depth image to unit magnitude ray vectors in the camera frame.
+     *
+     * @param[in]  stride coordinate in the non-contiguous image dimension
+     * @param[in]  step   coordinate in the contiguous image dimension
+     * @param[out] f      forward component of the ray associated with the image coordinate
+     * @param[out] r      right component of the ray associated with the image coordinate
+     * @param[out] d      down component of the ray associated with the image coordinate
+     *
+     * @note
+     * Invalid regions are identified by NAN outputs.
+     */
+    virtual void inverseProjection(const double& stride, const double& step, double& f, double& r, double& d) = 0;
     
     /**
      * Get index of first stride within the bounding box of the valid data region.
